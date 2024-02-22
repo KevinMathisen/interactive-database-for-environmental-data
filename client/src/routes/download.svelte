0aec9d0a67ddb6772bda1878c59764267c5612d8
Kevin Nikolai Mathisen <kevinnm@stud.ntnu.no>
2024-02-22 23:25:30 +0100

    50 - chore: change client structure from react to svelte

---
+++ client/src/routes/download.svelte
@@ -0,0 +1,1 @@
+<script> // Download page logic here </script><style> /* CSS here */ </style><div>Welcome to the Download Page</div>
---
+++ client/src/routes/upload.svelte
@@ -0,0 +1,1 @@
+<script> // Upload page logic here </script><style> /* CSS here */ </style><div>Welcome to the Upload Page</div>
