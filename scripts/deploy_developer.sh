3fddd71da077a71538bc13b66928d0c280c4eacd
Kevin Nikolai Mathisen <kevinnm@stud.ntnu.no>
2024-03-01 21:40:57 +0100

    70 - ops: update container name, paths, ports, and add sudo

--- scripts/deploy_developer.sh
+++ scripts/deploy_developer.sh
@@ -6,16 +6,17 @@ set -euo pipefail  # Exists the script if any command fail along the way
 cd /home/ubuntu/Interactive-Database-for-Environmental-Data
 
 # Pull the latest from the developer branch
+git checkout developer
 git pull origin developer
 
 # Stops and removes the container if it exists and ignores errors if it's not running
-docker stop staticwebserver:latest || true
-docker rm staticwebserver:latest || true
+sudo docker stop interactive_database:latest || true
+sudo docker rm interactive_database:latest || true
 
 # Build the Docker image
-docker build -t "staticwebserver:latest" ./web-server
+sudo docker build -t "interactive_database:latest" .
 
 # Run the Docker container
-docker run --restart=always -p 3000:3000 -d staticwebserver:latest
+sudo docker run --restart=always -p 80:80 -d interactive_database:latest
 
 echo "Svelte application sucessfully started on developer!"
