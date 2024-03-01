0a64b4da793ba3bf022cc67139641c4dc76396f9
martisgl <martisgl@stud.ntnu.no>
2024-03-01 11:56:37 +0100

    79 - feat: converted upload page to svelte

--- client/src/routes/download/+page.svelte
+++ client/src/routes/download/+page.svelte
@@ -1,6 +1,6 @@
-<script> // Download page logic here 
+<script> // Upload page logic here
 </script>
-    
+
 <div>Welcome to the Download Page</div>
 
 <style> /* CSS here */ </style>
--- client/src/routes/upload/+page.svelte
+++ client/src/routes/upload/+page.svelte
@@ -1,6 +1,83 @@
 <script> // Upload page logic here
- </script>
- 
-<div>Welcome to the Upload Page</div>
 
-<style> /* CSS here */ </style>
+</script>
+ 
+<div class="uploadBody">
+
+    <!--Defines the bos where you click to choose files -->
+    <div class="uploadFilesBox">
+        <img src="/uploadCloudIcon.svg" alt="listIcon" height="60px" id="uploadCloudIcon">
+        <p>Dra og slipp filer eller</p>
+        <div id="uploadFilesBoxButton" class="colorChange">Bla gjennom Filer</div>
+    </div>
+
+        <!-- Defines the text under the upload files box -->
+    <div class="uploadFilesBoxText">
+        <p>Maksimal fil størrelse: 10 MB</p>
+        <p>Kun tillatt å laste opp filer av typen: .csv and .xls</p>
+    </div>
+
+        <!-- Defines the overview over files selected -->
+    <div class="uploadFilesUploaded">
+        <p>Filer som er valgt:</p>
+    </div>
+</div>
+
+<!-- Defines the upload files button -->     
+<div class="uploadFilesButton">
+    <p>Last opp</p>
+</div>
+
+<style> 
+    .uploadBody {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    .uploadFilesBox {
+        border: 2px dashed black;
+        margin-top:3rem;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        width: 900px;
+        font-size: 1.5rem;
+    }
+
+    #uploadCloudIcon {
+        margin-top:1rem;
+        height: 100px;
+    }
+
+    #uploadFilesBoxButton {
+        background-color: #435768;
+        padding: 1rem 5rem 1rem 5rem;
+        margin: 3rem 0 1rem 0;
+        border-radius: 1rem;
+    }
+
+    .uploadFilesBoxText {
+        margin-right: 35rem;
+    }
+
+    .uploadFilesUploaded {
+        margin-top: 3rem;
+        font-size: 2rem;
+        height: 200px;
+        width: 900px;
+    }
+
+    .uploadFilesButton {
+        position: fixed;
+        right: 500px;
+        bottom: 100px;
+        font-size: 1.2rem;
+        background-color: tomato;
+        border-radius: 1rem;
+        width: 200px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
