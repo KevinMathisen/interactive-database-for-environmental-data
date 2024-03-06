aed887eca0a19c2ab767b6d5be4ddc22b8c32372
Kevin Nikolai Mathisen <kevinnm@stud.ntnu.no>
2024-03-06 19:23:17 +0100

    99 - test: add unit tests in postgrest module for function fetchStations

--- client/src/api/postgrest.test.js
+++ client/src/api/postgrest.test.js
@@ -83,8 +83,77 @@ describe('test fetchRivers function', {}, () => {
 		// Assert
 		expect(result).toBeNull();
 	});
+});
 
+describe('test fetchStations function', {}, () => {
+	beforeEach(() => {
+		// Clear all mocks
+		vi.clearAllMocks();
+	});
+
+	it('fetchStations should call fetch with the correct URL', async () => {
+		// Arrange
+		const endpoint = STATIONS_ENDPOINT;
+		const expectedUrl = `${POSTGREST_URL}/${endpoint}`;
+		const mockResponse = { data: 'example-data'};
+		global.fetch.mockResolvedValue({
+			ok: true,
+			json: vi.fn().mockResolvedValue(mockResponse),
+		});
+
+		// Act
+		await fetchStations();
+
+		// Assert
+		expect(global.fetch).toHaveBeenCalledWith(expectedUrl, { method: 'GET' });
+	});
+
+	it('fetchStations should return the correct response as JSON', async () => {
+		// Arrange
+		const mockResponse = { data: 'example-data'};
+		global.fetch.mockResolvedValue({
+			ok: true,
+			json: vi.fn().mockResolvedValue(mockResponse),
+		});
+
+		// Act
+		const result = await fetchStations();
+
+		// Assert
+		expect(result).toEqual(mockResponse);
+	});
+
+	it('fetchStations should throw an error if the response status is not ok', async () => {
+		// Arrange
+		const mockResponse = { statusText: 'Not Found' };
+		global.fetch.mockResolvedValue({
+			status: 404,
+			statusText: mockResponse.statusText,
+			json: vi.fn().mockResolvedValue(mockResponse),
+		});
+
+		// Act and Assert
+		await expect(fetchStations()).rejects.toThrowError(
+			mockResponse.statusText
+		);
+	});
 	
+	it('fetchStations should return null if the response status is 204', async () => {
+		// Arrange
+		const mockResponse = { statusText: 'No Content' };
+		global.fetch.mockResolvedValue({
+			status: 204,
+			ok: true,
+			statusText: mockResponse.statusText,
+			json: vi.fn().mockResolvedValue(mockResponse),
+		});
+
+		// Act
+		const result = await fetchStations();
+
+		// Assert
+		expect(result).toBeNull();
+	});
 });
 
 
