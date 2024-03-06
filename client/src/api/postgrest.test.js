9a0993822da5b86f86f4a7927a3a99bd8895515c
Kevin Nikolai Mathisen <kevinnm@stud.ntnu.no>
2024-03-06 19:24:27 +0100

    99 - test: add unit tests in postgrest module for function fetchStationDownload

--- client/src/api/postgrest.test.js
+++ client/src/api/postgrest.test.js
@@ -318,5 +318,93 @@ describe('test fetchStationSummary function', {}, () => {
 	});
 });
 
+describe('test fetchStationDownload function', {}, () => {
+	beforeEach(() => {
+		// Clear all mocks
+		vi.clearAllMocks();
+	});
 
+	it('fetchStationDownload should call fetch with the correct URL when 1 id', async () => {
+		// Arrange
+		const id = [1];
+		const endpoint = `${STATION_DOWNLOAD_ENDPOINT}?id=eq.${id[0]}`;
+		const expectedUrl = `${POSTGREST_URL}/${endpoint}`;
+		const mockResponse = { data: 'example-data'};
+		global.fetch.mockResolvedValue({
+			ok: true,
+			json: vi.fn().mockResolvedValue(mockResponse),
+		});
+
+		// Act
+		await fetchStationDownload(id);
+
+		// Assert
+		expect(global.fetch).toHaveBeenCalledWith(expectedUrl, { method: 'GET' });
+	});
+
+	it('fetchStationDownload should call fetch with the correct URL when multiple ids', async () => {
+		// Arrange
+		const id = [1, 2, 3];
+		const endpoint = `${STATION_DOWNLOAD_ENDPOINT}?id=in.(${id.join(',')})`;
+		const expectedUrl = `${POSTGREST_URL}/${endpoint}`;
+		const mockResponse = { data: 'example-data'};
+		global.fetch.mockResolvedValue({
+			ok: true,
+			json: vi.fn().mockResolvedValue(mockResponse),
+		});
+
+		// Act
+		await fetchStationDownload(id);
+
+		// Assert
+		expect(global.fetch).toHaveBeenCalledWith(expectedUrl, { method: 'GET' });
+	});
+
+	it('fetchStationDownload should return the correct response as JSON', async () => {
+		// Arrange
+		const mockResponse = { data: 'example-data'};
+		global.fetch.mockResolvedValue({
+			ok: true,
+			json: vi.fn().mockResolvedValue(mockResponse),
+		});
+
+		// Act
+		const result = await fetchStationDownload([1]);
+
+		// Assert
+		expect(result).toEqual(mockResponse);
+	});
+
+	it('fetchStationDownload should throw an error if the response status is not ok', async () => {
+		// Arrange
+		const mockResponse = { statusText: 'Not Found' };
+		global.fetch.mockResolvedValue({
+			status: 404,
+			statusText: mockResponse.statusText,
+			json: vi.fn().mockResolvedValue(mockResponse),
+		});
+
+		// Act and Assert
+		await expect(fetchStationDownload([1])).rejects.toThrowError(
+			mockResponse.statusText
+		);
+	});
+
+	it('fetchStationDownload should return null if the response status is 204', async () => {
+		// Arrange
+		const mockResponse = { statusText: 'No Content' };
+		global.fetch.mockResolvedValue({
+			status: 204,
+			ok: true,
+			statusText: mockResponse.statusText,
+			json: vi.fn().mockResolvedValue(mockResponse),
+		});
+
+		// Act
+		const result = await fetchStationDownload([1]);
+
+		// Assert
+		expect(result).toBeNull();
+	});
+});
 
