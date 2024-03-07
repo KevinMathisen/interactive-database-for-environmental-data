87cfb2c156e49720c5433bc0079048cb24d618d5
Kevin Nikolai Mathisen <kevinnm@stud.ntnu.no>
2024-03-07 14:18:39 +0100

    88 - feat: add feedbackmessages for service unavailable

--- client/src/constants/feedbackMessages.js
+++ client/src/constants/feedbackMessages.js
@@ -1,10 +1,10 @@
 export const FEEDBACK_TYPES = {
   SUCCESS: 'SUCCESS',
   INFO: 'INFO',
-  ERROR: 'ERROR',
+  ERROR: 'ERROR'
 }
 
-export const FEEDBACK_CODES = {	
+export const FEEDBACK_CODES = {
   CREATED: 'CREATED',
   NO_CONTENT: 'NO_CONTENT',
   INFO: 'INFO',
@@ -14,15 +14,19 @@ export const FEEDBACK_CODES = {
   UNAUTHORIZED: 'UNAUTHORIZED',
   CONTENT_TO_LARGE: 'CONTENT_TO_LARGE',
   UNSUPPORTED_CONTENT_TYPE: 'UNSUPPORTED_CONTENT_TYPE',
+  POSTGREST_UNAVAILABLE: 'POSTGREST_UNAVAILABLE',
+  AUTH_UNAVAILABLE: 'AUTH_UNAVAILABLE'
 }
 
 export const FEEDBACK_MESSAGES = {
-	CREATED: 'Opplasting vellykket',
-	GENERIC: 'En feil oppstod',
-	NO_CONTENT: 'Fant ikke det du lette etter',
-	NOT_FOUND: 'Kunne ikke finne den forespurte ressursen',
-	FORBIDDEN: 'Handlingen du utførte ble ikke akseptert',
-	UNAUTHORIZED: 'Du har ikke tillatelse til å utføre handlingen du forsøkte',
-	CONTENT_TO_LARGE: 'Filen du forsøkte å laste opp er for stor',
-	UNSUPPORTED_CONTENT_TYPE: 'Filtypen du forsøkte å laste opp støttes ikke',
+  CREATED: 'Opplasting vellykket',
+  GENERIC: 'En feil oppstod',
+  NO_CONTENT: 'Fant ikke det du lette etter',
+  NOT_FOUND: 'Kunne ikke finne den forespurte ressursen',
+  FORBIDDEN: 'Handlingen du utførte ble ikke akseptert',
+  UNAUTHORIZED: 'Du har ikke tillatelse til å utføre handlingen du forsøkte',
+  CONTENT_TO_LARGE: 'Filen du forsøkte å laste opp er for stor',
+  UNSUPPORTED_CONTENT_TYPE: 'Filtypen du forsøkte å laste opp støttes ikke',
+  POSTGREST_UNAVAILABLE: 'Kunne ikke hente data fra serveren, prøv igjen senere',
+  AUTH_UNAVAILABLE: 'Kunne ikke autentisere brukeren, prøv igjen senere'
 }
