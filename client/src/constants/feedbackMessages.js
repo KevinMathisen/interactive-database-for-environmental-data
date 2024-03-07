58a71bcbc2196cbf0f9218a336dadd3a609c4a37
Kevin Nikolai Mathisen <kevinnm@stud.ntnu.no>
2024-03-07 13:57:51 +0100

    88 - feat: add constants for user feedback messages

---
+++ client/src/constants/feedbackMessages.js
@@ -0,0 +1,21 @@
+export const FEEDBACK_CODES = {	
+  GENERIC: 'GENERIC',
+  CREATED: 'CREATED',
+  NO_CONTENT: 'NO_CONTENT',
+  NOT_FOUND: 'NOT_FOUND',
+  FORBIDDEN: 'FORBIDDEN',
+  UNAUTHORIZED: 'UNAUTHORIZED',
+  CONTENT_TO_LARGE: 'CONTENT_TO_LARGE',
+  UNSUPPORTED_CONTENT_TYPE: 'UNSUPPORTED_CONTENT_TYPE',
+}
+
+export const FEEDBACK_MESSAGES = {
+	GENERIC: 'En feil oppstod',
+	CREATED: 'Opplasting vellykket',
+	NO_CONTENT: 'Fant ikke det du lette etter',
+	NOT_FOUND: 'Kunne ikke finne den forespurte ressursen',
+	FORBIDDEN: 'Handlingen du utførte ble ikke akseptert',
+	UNAUTHORIZED: 'Du har ikke tillatelse til å utføre handlingen du forsøkte',
+	CONTENT_TO_LARGE: 'Filen du forsøkte å laste opp er for stor',
+	UNSUPPORTED_CONTENT_TYPE: 'Filtypen du forsøkte å laste opp støttes ikke',
+}
