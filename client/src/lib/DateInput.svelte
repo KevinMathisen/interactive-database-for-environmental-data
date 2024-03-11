df748150636891124c4a9d07247729ba9559b75e
Kevin Nikolai Mathisen <kevinnm@stud.ntnu.no>
2024-03-11 15:46:08 +0100

    82 - feat: create component for choosing start and end dates

---
+++ client/src/lib/DateInput.svelte
@@ -0,0 +1,23 @@
+<script>
+	export let selectedStartDate;	// Start date
+	export let selectedEndDate;		// End date
+</script>
+
+<!-- Input for choosing start date -->
+<label for="startDate">
+	Fra
+	<input id="startDate" type="date" name="startDate" bind:value={selectedStartDate}/>
+</label>
+<!-- Input for choosing end date -->
+<label for="endDate">
+	Til
+	<input id="endDate" type="date" name="endDate" bind:value={selectedEndDate}/>
+</label>
+
+<style>
+	label {
+		display: block;
+		padding: 0.5em;
+		font-size: 1.2rem;
+	}
+</style>
