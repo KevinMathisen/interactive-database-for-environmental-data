5a1cfcbb58a51d9bd3e04d6720bbe67dbbe5c488
Kevin Nikolai Mathisen <kevinnm@stud.ntnu.no>
2024-03-11 15:45:00 +0100

    82 - feat: create component for choosing species

---
+++ client/src/lib/SpeciesInput.svelte
@@ -0,0 +1,130 @@
+<script>
+	export let selectableSpecies = [];	// Get species the user can choose from
+
+	export let chooseAll = true;		// If the user wants to choose all species
+	export let customSpecies = [];		// Custom species the user has chosen
+
+	let inputSpecies = "";				// Input in add species field
+	let showSuggestions = false;		// If suggestions should be displayed
+	let showError = "";					// Error message to display to the user
+	let suggestSpecies = [];			// Species to suggest to the user
+
+	/**
+	 * Adds the species the user has written in the input field to the custom species
+	 */
+	function addSpecies() {
+		let capitalizedInputSpecies = capitalizeFirstLetter(inputSpecies);
+
+		// If the input is empty, do nothing
+		if (capitalizedInputSpecies.trim().length === 0) {
+			return;
+		}
+
+		// If the input is not a selectable species, display an error
+		if (!selectableSpecies.includes(capitalizedInputSpecies)) {
+			displayError("Art finnes ikke");
+			return;
+		}
+
+		// If the input is already chosen, display an error
+		if (customSpecies.includes(capitalizedInputSpecies)) {
+			displayError("Art allerede valgt");
+			return;
+		}
+
+		// Remove error message, add the species to the custom species and reset the input field
+		displayError("");		
+		customSpecies = [...customSpecies, capitalizedInputSpecies];
+		inputSpecies = "";
+	}
+
+	/**
+	 * Displays an error message to the user
+	 * @param message - The message to display
+	 */
+	function displayError(message) {
+		showError = message;
+	}
+
+	/**
+	 * Removes a species from the custom species
+	 * @param speciesToRemove - The species to remove
+	 */
+	function removeSpecies(speciesToRemove) {
+		customSpecies = customSpecies.filter((species) => species !== speciesToRemove);
+	}
+
+	/**
+	 * Handles the keydown event on the input field, 
+	 *    adds a species if the user presses enter
+	 * @param event - The event
+	 */
+	function handleKeydown(event) {
+		if (event.key === "Enter") {
+			addSpecies();
+		}
+	}
+
+	/**
+	 * Capitalizes the first letter of a string
+	 * @param string - The string to capitalize
+	 * @returns The string with the first letter capitalized
+	 */
+	function capitalizeFirstLetter(string) {
+		return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+	}
+
+	// Get the species to suggest to the user based on the input
+	$: suggestSpecies = selectableSpecies.filter((species) => 
+		species.toLowerCase().includes(inputSpecies.toLowerCase())
+		&& !customSpecies.includes(species)
+	);
+
+	// If the input is not empty and there are suggestions, display them
+	$: showSuggestions = inputSpecies.trim().length > 0 && suggestSpecies.length > 0;
+
+</script>
+
+<!-- Input for choosing all or custom species -->
+<label>
+	<input type="radio" bind:group={chooseAll} value={true}> Velg alle
+</label>
+<label>
+	<input type="radio" bind:group={chooseAll} value={false}> Egendefinert
+</label>
+
+<!-- Input for handling custom species -->
+{#if !chooseAll}
+	<!-- Input for adding a species to the custom species-->
+	<input 
+		type="text" 
+		bind:value={inputSpecies} 
+		on:keydown={handleKeydown}
+		placeholder="Legg til art"/>
+	<button on:click={addSpecies}>+</button>
+
+	<!-- Error message to display to the user -->
+	{#if showError}
+		<p>{showError}</p>
+	{/if}
+
+	<!-- Suggestions based on user input -->
+	{#if showSuggestions}
+		<ul>
+			{#each filteredSpecies as species}
+				<li on:click={() => {inputSpecies = capitalizeFirstLetter(species); addSpecies()}}>{capitalizeFirstLetter(species)}</li>
+			{/each}
+		</ul>
+	{/if}
+
+	<!-- Custom species choosen by user -->
+	<p>Valgte arter:</p>
+	<ul>
+		{#each customSpecies as species}
+			<li>{capitalizeFirstLetter(species)} <button on:click={() => removeSpecies(species)}>x</button></li>
+		{/each}
+	</ul>
+{/if}
+
+
+
