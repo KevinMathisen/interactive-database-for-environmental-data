fa2a5aa80e19ea0f83b69cfcf0db3f1fa914ab66
Kevin Nikolai Mathisen <kevinnm@stud.ntnu.no>
2024-03-12 21:24:04 +0100

    85 - feat: set up graph page html structure

--- client/src/routes/graph/+page.svelte
+++ client/src/routes/graph/+page.svelte
@@ -1,44 +1,49 @@
 <script> // Graph page logic here 
-    import Graph from '$lib/optionsGraph.svelte';
+    import GraphFilter from '$lib/GraphFilter.svelte'
+	import Sidebar from '../../lib/Sidebar.svelte';
     import PlotlyComponent from './PlotlyComponent.svelte';
 </script>
     
-<div class="graphMain">
-    <Graph/>
-
-    <div class="graphBox1">
-        <h3>FORDELING AV ARTER</h3>
-        <PlotlyComponent type={'bar'}/>                 <!--Bar chart-->
+<div class="graphPage">
+    <div class="filterContainer">
+        <Sidebar title="Filter for Grafer" typeClose="sideButton">
+            <GraphFilter />
+        </Sidebar>
     </div>
-
-    <div class="graphBox2">
-        <h3>FORDELING AV LENGDE</h3>
-        <PlotlyComponent type={'box'}/>                 <!--Box plot-->
+    
+    <div class="graphMain">
+        <div class="graphBox1">
+            <h3>FORDELING AV ARTER</h3>
+            <PlotlyComponent type={'bar'}/>
+        </div>
+    
+        <div class="graphBox2">
+            <h3>FORDELING AV LENGDE</h3>
+            <PlotlyComponent type={'box'}/>
+        </div>
     </div>
+    
 </div>
 
 
 <style> 
-    .graphMain {
-        display: grid;
-        flex-direction: row;
-        grid-template-areas: 
-        "filter graph1"                   
-        "filter graph2";
-        grid-template-rows: 1fr 1fr;
-        grid-template-columns: 305px 1fr;  /* Width of the filter, 305 since the border is 5 and filter 300*/
+    .graphPage {
+        height: calc(100vh - 80px); 
+        width: 100%;
+        display: flex;
+    }
+
+    .filterContainer {
+        width: 300px;
+        height: 100%;
     }
     
-    .graphBox1 {
-        grid-area: graph1;
+    .graphMain {
+        flex-grow: 1;
     }
 
     .graphBox1 h3 {
         padding: 1rem 0 0 8rem;
-    }
-
-    .graphBox2 {
-        grid-area: graph2;
     }
 
     .graphBox2 h3 {
