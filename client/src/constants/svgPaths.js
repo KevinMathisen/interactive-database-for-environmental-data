ade694345b287e626577413fc32ecb9d1b0f72d1
Carl Petter Morch-Reiersen <carlpettermr@gmail.com>
2024-03-12 19:00:19 +0100

    96 - fix: fixed syntax error for pathing

--- client/src/constants/svgPaths.js
+++ client/src/constants/svgPaths.js
@@ -1,10 +1,9 @@
-const svgBasePath = '/static/'; // Assuming your SVGs are stored in /static/
+const svgBasePath = '/'; // Assuming your SVGs are stored in /static/
 
 export const SVG_PATHS = {
     error: `${svgBasePath}errorIcon.svg`,
     success: `${svgBasePath}successIcon.svg`,
-    close: `${svgBasePath}closeIcon.svg`,
+    close: `${svgBasePath}closeIcon.svg`
 };
 
-export default SVG_PATHS;
 
