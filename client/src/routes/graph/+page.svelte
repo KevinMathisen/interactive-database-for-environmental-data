f198d9fc54de7adde0adb7ed7b61a8f863f911f0
Kevin Nikolai Mathisen <kevinnm@stud.ntnu.no>
2024-03-13 11:40:27 +0100

    85 - feat: import and filter data on map page

--- client/src/routes/graph/+page.svelte
+++ client/src/routes/graph/+page.svelte
@@ -2,25 +2,84 @@
     import GraphFilter from '$lib/GraphFilter.svelte'
 	import Sidebar from '../../lib/Sidebar.svelte';
     import PlotlyComponent from './PlotlyComponent.svelte';
+    import { riverStore } from '../../stores/riverStore.js';
+    import { stationStore } from '../../stores/stationStore.js';
+    import { getRivers, getStations } from '../../utils/dataManager.js';
+    import { getSelectableSpecies } from '../../utils/filterData.js';
+    
+
+    let rivers = new Map();
+    let stations = new Map();
+    let selectedRivers;
+    let selectedStations;
+    let selectableSpecies;
+
+    let dataType;
+    let selectedSpecies;
+
+    let showPlotA;
+    let showValueA;
+    let plotTypeA;
+
+    let showPlotB;
+    let intervallPlotB;
+    let plotTypeB;
+
+    // Get rivers and stations from stores
+    $: rivers = $riverStore;
+    $: stations = $stationStore;
+
+    // Get selectable species
+    $: selectableSpecies = dataType === 'river' ? getSelectableSpecies(rivers) : getSelectableSpecies(stations);
+
+    function onSelectRiverStation() {
+        // should get the selected rivers and stations from event
+        if (dataType === 'river') {
+            selectedRivers = new Map(rivers[3])
+            selectableSpecies = getSelectableSpecies(rivers)
+        } else {
+            selectedStations = new Map(stations[11])
+            selectableSpecies = getSelectableSpecies(stations)
+        }
+    }
+
 </script>
     
 <div class="graphPage">
     <div class="filterContainer">
         <Sidebar title="Filter for Grafer" typeClose="sideButton">
-            <GraphFilter />
+            <GraphFilter 
+                {selectedRivers}
+                {selectedStations}
+                {selectableSpecies}
+                bind:dataType
+                bind:selectedSpecies
+                bind:showPlotA
+                bind:showValueA
+                bind:plotTypeA
+                bind:showPlotB
+                bind:intervallPlotB
+                bind:plotTypeB
+                />
         </Sidebar>
     </div>
     
     <div class="graphMain">
-        <div class="graphBox1">
-            <h3>FORDELING AV ARTER</h3>
-            <PlotlyComponent type={'bar'}/>
-        </div>
+        <button on:click={onSelectRiverStation}>Velg elver/stasjoner</button>
+
+        {#if showPlotA}
+            <div class="graphBox1">
+                <h3>FORDELING AV ARTER</h3>
+                <PlotlyComponent type={plotTypeA}/>
+            </div>
+        {/if}
     
-        <div class="graphBox2">
-            <h3>FORDELING AV LENGDE</h3>
-            <PlotlyComponent type={'box'}/>
-        </div>
+        {#if showPlotB}
+            <div class="graphBox2">
+                <h3>FORDELING AV LENGDE</h3>
+                <PlotlyComponent type={plotTypeB}/>
+            </div>
+        {/if}
     </div>
     
 </div>
@@ -40,6 +99,8 @@
     
     .graphMain {
         flex-grow: 1;
+        padding: 4em;
+        overflow:auto;
     }
 
     .graphBox1 h3 {
