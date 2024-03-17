cb2a0e9d8d37addcd1291cc2d63c996a1549bfc3
Carl Petter Morch-Reiersen <carlpettermr@gmail.com>
2024-03-17 14:43:33 +0100

    100 - style: fix linting issues

--- client/src/stores/stationStore.js
+++ client/src/stores/stationStore.js
@@ -1,3 +1,3 @@
-import { writable } from 'svelte/store'
+import { writable } from 'svelte/store';
 
-export const stationStore = writable(new Map())
+export const stationStore = writable(new Map());
