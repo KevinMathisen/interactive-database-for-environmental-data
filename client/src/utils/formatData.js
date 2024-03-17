29c82effc842f1ee33d2312e1bfe181fd89051e7
Carl Petter Morch-Reiersen <carlpettermr@gmail.com>
2024-03-17 14:51:10 +0100

    100 - style: add jsdoc standard

--- client/src/utils/formatData.js
+++ client/src/utils/formatData.js
@@ -1,46 +1,44 @@
-import { addFeedbackToStore } from './addFeedbackToStore.js'
-import { FEEDBACK_TYPES, FEEDBACK_CODES, FEEDBACK_MESSAGES } from '../constants/feedbackMessages'
+//import { addFeedbackToStore } from './addFeedbackToStore.js';
+//import { FEEDBACK_TYPES, FEEDBACK_CODES, FEEDBACK_MESSAGES } from '../constants/feedbackMessages';
 
 /**
  * Converts river objects into arrays for display in a table
- * 
- * @param {Map<int, River>} rivers - The rivers to convert to array
+ * @param {Map<number, object>} rivers - The rivers to convert to array
  * @returns {{headers: string[], rows: string[][]}} - Headers and rows for the table
  */
 export function formatRiversForTable (rivers) {
-  let headers = ['Navn', 'Dato', 'Projektnummer']
+  let headers = ['Navn', 'Dato', 'Projektnummer'];
 
-  let rows = []
+  let rows = [];
   rivers.forEach(river => {
     rows.push([
       river.id,
       river.name,
       river.startDate,
       river.projectId
-    ])
-  })
+    ]);
+  });
 
-  return {headers, rows}
+  return {headers, rows};
 }
 
 /**
  * Converts station objects into arrays for display in a table
- * 
- * @param {Map<int, Station>} stations - The stations to convert to array
+ * @param {Map<number, object>} stations - The stations to convert to array
  * @returns {{headers: string[], rows: string[][]}} - Headers and rows for the table
  */
 export function formatStationsForTable (stations) {
-  let headers = ['Navn', 'Dato', 'Kl.']
+  let headers = ['Navn', 'Dato', 'Kl.'];
   
-  let rows = []
+  let rows = [];
   stations.forEach(station => {
     rows.push([
       station.id,
       station.name,
       station.date,
       station.time
-    ])
-  })
+    ]);
+  });
 
-  return {headers, rows}
+  return {headers, rows};
 }
