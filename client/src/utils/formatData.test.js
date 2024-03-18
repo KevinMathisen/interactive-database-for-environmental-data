2914e64a526c2735373a1eb04d9d0b93cb533dd2
Kevin Nikolai Mathisen <kevinnm@stud.ntnu.no>
2024-03-18 11:34:33 +0100

    129 - test: create unit tests for formatStationsForCsv(...)

--- client/src/utils/formatData.test.js
+++ client/src/utils/formatData.test.js
@@ -257,3 +257,65 @@ describe('test formatRiversForCsv function', () => {
   })
 })
 
+describe('test formatStationsForCsv function', () => {
+  beforeEach(() => {
+    vi.resetModules()
+    vi.clearAllMocks()
+  })
+
+  it('should return empty rows and correct headers if the input map is empty', () => {
+    const formatted = formatStationsForCsv(new Map())
+
+    expect(formatted.header).toEqual([
+      ...headersConstants.RIVER_HEADERS_EXCEL, 
+      ...headersConstants.STATION_HEADERS_EXCEL, 
+      ...headersConstants.OBSERVATION_HEADERS_EXCEL
+    ])
+    expect(formatted.rows).toEqual([])
+  })
+
+  it('should correctly convert station objects into arrays for CSV display', () => {
+    const rivers = new Map([
+      [1, new River({ 
+        id: 1, name: 'Name1', start_date: '2024-01-01', project_id: 'prosjekt1', 
+        pos: {coordinates: [9.0, 60.5]}, stations: [1]})],
+      [2, new River({ 
+        id: 2, name: 'Name2', start_date: '2024-01-15', project_id: 'prosjekt2', 
+        pos: {coordinates: [9.1, 60.6]}, stations: [2]})]
+    ])
+    const stations = new Map([
+      [1, new Station({ 
+        id: 1, name: 'Station 1', date: '2024-01-01', time: '12:00',
+        water_temp: 4, comment: 'Comment1', 
+        start_pos: {coordinates: [9.0, 60.5]}, end_pos: {coordinates: [9.01, 60.51]},
+        observations: [
+          { id: 1, station: 1, species: 'Species1', length: 2, count: 1, comment: 'Comment1' }
+        ], river_id: 1})],
+      [2, new Station({
+        id: 2, name: 'Station 2', date: '2024-01-15', time: '13:00',
+        water_temp: 6, comment: 'Comment2',
+        start_pos: {coordinates: [9.1, 60.6]}, end_pos: {coordinates: [9.11, 60.61]},
+        observations: [
+          { id: 2, station: 2, species: 'Species2', length: 3, count: 1, comment: 'Comment2' }
+        ], river_id: 2})
+      ]
+    ])
+    vi.mocked(get).mockReturnValue(rivers)
+
+    const formatted = formatStationsForCsv(stations)
+
+    expect(formatted.header).toEqual([
+      ...headersConstants.RIVER_HEADERS_EXCEL, 
+      ...headersConstants.STATION_HEADERS_EXCEL, 
+      ...headersConstants.OBSERVATION_HEADERS_EXCEL
+    ])
+    expect(formatted.rows).toEqual([
+      ['2024-01-01', '', 'Name1', '', 60.5, 9.0, '', '', '', '', '', 'prosjekt1', '', 
+      '1', '2024-01-01', '12:00', 60.5, 9, 60.51, 9.01, '', '', 4, '', '', '', '', '', '', '', '', '', 'Comment1',
+      1, 1, '', 'Species1', 2, 1, '', '', '', '', 'Comment1'],
+      ['2024-01-15', '', 'Name2', '', 60.6, 9.1, '', '', '', '', '', 'prosjekt2', '',
+      '2', '2024-01-15', '13:00', 60.6, 9.1, 60.61, 9.11, '', '', 6, '', '', '', '', '', '', '', '', '', 'Comment2',
+      2, 2, '', 'Species2', 3, 1, '', '', '', '', 'Comment2']
+    ])
+  })
+})
