3935a65935c5297889ae59f02bbe8924b4a89d3a
Kevin Nikolai Mathisen <kevinnm@stud.ntnu.no>
2024-03-18 10:09:26 +0100

    129 - feat: add functions createRowForRiver/Station/Observation to create rows for excel file download

--- client/src/utils/formatData.js
+++ client/src/utils/formatData.js
@@ -1,5 +1,9 @@
 // import { addFeedbackToStore } from './addFeedbackToStore.js';
 // import { FEEDBACK_TYPES, FEEDBACK_CODES, FEEDBACK_MESSAGES } from '../constants/feedbackMessages';
+import headersConstants from '../constants/headers.js';
+import { riverStore } from '../stores/riverStore.js'
+import { stationStore } from '../stores/stationStore.js'
+import { get } from 'svelte/store'
 
 /**
  * Converts river objects into arrays for display in a table
@@ -7,7 +11,7 @@
  * @returns {{headers: string[], rows: string[][]}} - Headers and rows for the table
  */
 export function formatRiversForTable (rivers) {
-  const headers = ['Navn', 'Dato', 'Projektnummer']
+  const headers = headersConstants.RIVER_HEADERS_TABLE
 
   const rows = []
   rivers.forEach(river => {
@@ -28,7 +32,7 @@ export function formatRiversForTable (rivers) {
  * @returns {{headers: string[], rows: string[][]}} - Headers and rows for the table
  */
 export function formatStationsForTable (stations) {
-  const headers = ['Navn', 'Dato', 'Kl.']
+  const headers = headersConstants.STATION_HEADERS_TABLE
 
   const rows = []
   stations.forEach(station => {
@@ -41,4 +45,50 @@ export function formatStationsForTable (stations) {
   })
 
   return { headers, rows }
+}
+
+/**
+ * Converts a river object into a string array for insertion into an Excel file
+ * Each string is an attribute of the object and a column in the Excel file 
+ * 
+ * @param {River} river - The river to create a row for
+ * @returns {string[]} - A row in excel
+ */
+function createRowForRiver(river) {
+  return [
+    river.startDate, river.endDate, river.name, river.boatType, river.position.coordinates[1],
+    river.position.coordinates[0], river.waterflow, river.skipper, river.crew[0], river.crew[1], river.crew[2],
+    river.projectId, river.comment
+  ]
+}
+
+/**
+ * Converts a station object into a string array for insertion into an Excel file
+ * Each string is an attribute of the object and a column in the Excel file 
+ * 
+ * @param {Station} station - The station to create a row for
+ * @returns {string[]} - A row in excel
+ */
+function createRowForStation(station) {
+  return [
+    row.name.split(' ')[1], row.date, row.time, row.startPos.coordinates[0], row.startPos.coordinates[1],
+    row.endPos.coordinates[0], row.endPos.coordinates[1], row.riverType, row.weather, row.waterTemp, row.airTemp,
+    row.conductivity, row.transectLength, row.secFished, row.voltage, row.pulse, row.display, row.gpxFile,
+    row.description, row.comment
+  ]
+}
+
+/**
+ * Converts an observation object into a string array for insertion into an Excel file
+ * Each string is an attribute of the object and a column in the Excel file 
+ * 
+ * @param {Observation} observation - The observation to create a row for
+ * @returns {string[]} - A row in excel
+ */
+function createRowForObservation(observation) {
+  return [
+    observation.id, observation.station, observation.round, observation.species, 
+    observation.length, observation.count, observation.gender, observation.age, observation.released,
+    observation.sampleType, observation.comment
+  ]
 }
