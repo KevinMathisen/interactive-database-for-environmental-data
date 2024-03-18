18195bd571342e85168ecb9544fb66ba5be970af
Kevin Nikolai Mathisen <kevinnm@stud.ntnu.no>
2024-03-18 10:19:46 +0100

    129 - feat: create header name constants

---
+++ client/src/constants/headers.js
@@ -0,0 +1,22 @@
+const RIVER_HEADERS_TABLE = ['Navn', 'Dato', 'Projektnummer']
+const STATION_HEADERS_TABLE = ['Navn', 'Dato', 'Kl.']
+
+const RIVER_HEADERS_EXCEL = [
+  'Start dato', 'Slutt dato', 'Elv', 'Båttype', 'Lat', 'Long',
+  'Vannføring (sildre.no)', 'Skipper', 'Mannskap1', 'Mannskap2', 
+  'Mannskap3', 'Prosjekt', 'Prosjektnummer', 'Kommentar'
+]
+const STATION_HEADERS_EXCEL = [
+  'Stasjon', 'Båttype', 'Dato', 'Klokkeslett start', 'Lat start', 'Long start', 
+  'Lat stopp', 'Long stopp', 'Dominerende elvtype', 'Vær', 
+  'Vanntemperatur (Celsius)', 'Lufttemperatur (Celsius)', 
+  'Ledningsevne (µs/cm)', 'Transektlengde (m)', 'Sekunder fisket (s)', 'Volt', 
+  'Puls (DC)', 'Oservasjoner', 'Display', 'Gpx File', 'Stasjonsbeskrivelse', 
+  'Kommentar til fiske (observasjoner osv)'
+]
+const OBSERVATION_HEADERS_EXCEL = [
+  'ID', 'Stasjon', 'Omgang', 'Art', 'Lengde', 'Antall', 'Kjønn', 'Alder', 
+  'Gjenutsatt(ja/nei)', 'Prøvetatt(ja/nei)', 'Kommentar'
+]
+
+export default { RIVER_HEADERS_TABLE, STATION_HEADERS_TABLE, RIVER_HEADERS_EXCEL, STATION_HEADERS_EXCEL, OBSERVATION_HEADERS_EXCEL }
