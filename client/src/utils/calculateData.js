db24549594fd5f7b2be3239d718c9bf4b9bc2c35
Kevin Nikolai Mathisen <kevinnm@stud.ntnu.no>
2024-03-19 17:34:05 +0100

    115 - feat: create calculate functions secondsSpentFishingInStations(...) and secondsAndMinutesSpentFishingInStation(...)

--- client/src/utils/calculateData.js
+++ client/src/utils/calculateData.js
@@ -18,3 +18,27 @@ export function amountOfFishInStations (stations) {
   return Array.from(stations.values()).reduce((amountOfFish, station) => amountOfFish + amountOfFishInStation(station), 0)
 }
 
+/**
+ * Calculates the time spent fishing in multiple stations
+ * 
+ * @param {Map<number, Station>} station 
+ * @returns {number} - The time spent fishing in all the stations
+ */
+function secondsSpentFishingInStations (stations) {
+  return Array.from(stations.values()).reduce((secSpentFishing, station) => secSpentFishing + station.secFished, 0)
+}
+
+/**
+ * Calculates the minutes and seconds which together represent the time spent fishing 
+ * 
+ * @param {Map<number, Station>} stations - The stations to calculate the time spent fishing in
+ * @returns {number} - The minutes and seconds spent fishing in all the stations
+ */
+export function secondsAndMinutesSpentFishingInStations (stations) {
+  const secSpentFishing = secondsSpentFishingInStations(stations)
+  return {
+    seconds: secSpentFishing % 60,
+    minutes: Math.floor(secSpentFishing / 60)
+  }
+}
+
