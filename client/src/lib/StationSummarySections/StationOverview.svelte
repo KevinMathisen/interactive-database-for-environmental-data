d67a58d6bbdfe92ee532fcfc16f8631d8ab9dd48
Kevin Nikolai Mathisen <kevinnm@stud.ntnu.no>
2024-03-20 14:19:23 +0100

    116 - feat: create component for displaying overview info of station

---
+++ client/src/lib/StationSummarySections/StationOverview.svelte
@@ -0,0 +1,30 @@
+<script>
+  import Button from "../Button.svelte"
+  
+  export let station // Station to show
+</script>
+
+<div class='container'>
+  <div class='text'>
+    <p>{station.date} - {station.time}</p>
+    <p>Prosjektnummer: TODO</p>
+  </div>
+
+  <!-- Button to go to river data -->
+  <Button color='blue' type='small'>
+    Til elvedata
+  </Button>
+  
+</div>
+
+<style>
+  .container {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    height: 100%;
+    width: 100%;
+  }
+  
+</style>
