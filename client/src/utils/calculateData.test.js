ada27910bc8f8e61b1da0e6ec4b94870b2b1939a
Kevin Nikolai Mathisen <kevinnm@stud.ntnu.no>
2024-03-20 18:34:46 +0100

    131 - test: create unit tests for secondsAndMinutesSpentFishingInStations(...), fishPerMinuteInStation(s)(...)

--- client/src/utils/calculateData.test.js
+++ client/src/utils/calculateData.test.js
@@ -45,8 +45,57 @@ describe('test amountOfFishInStations function', () => {
   })
 })
 
+describe('test secondsAndMinutesSpentFishingInStations function', () => {
+  it('should return 0 minutes and 0 seconds when no stations are present', () => {
+    const stations = new Map()
+    expect(secondsAndMinutesSpentFishingInStations(stations)).toEqual({ seconds: 0, minutes: 0 })
+  })
 
+  it('should return the correct amount of minutes', () => {
+    const stations = new Map()
+    stations.set(1, new Station({ secFished: 120 }))
+    stations.set(2, new Station({ secFished: 180 }))
+    expect(secondsAndMinutesSpentFishingInStations(stations)).toEqual({ seconds: 0, minutes: 5 })
+  })
 
+  it('should return the correct amount of seconds', () => {
+    const stations = new Map()
+    stations.set(1, new Station({ secFished: 120 }))
+    stations.set(2, new Station({ secFished: 121 }))
+    expect(secondsAndMinutesSpentFishingInStations(stations)).toEqual({ seconds: 1, minutes: 4 })
+  })
+})
 
+describe('test fishPerMinuteInStation function', () => {
+  it('should return 0 when no fish are observed', () => {
+    const station = new Station({ observations: [], secFished: 60 })
+    expect(fishPerMinuteInStation(station)).toBe('0.00')
+  })
 
+  it('should return the correct amount of fish per minute', () => {
+    const station = new Station({ observations: [{ count: 60 }], secFished: 60 })
+    expect(fishPerMinuteInStation(station)).toBe('60.00')
+  })
+})
+
+describe('test fishPerMinuteInStations function', () => {
+  it('should return 0 when no fish are observed', () => {
+    const stations = new Map()
+    stations.set(1, new Station({ observations: [], secFished: 60 }))
+    expect(fishPerMinuteInStations(stations)).toBe('0.00')
+  })
+
+  it('should return the correct amount of fish per minute when 1 station', () => {
+    const stations = new Map()
+    stations.set(1, new Station({ observations: [{ count: 60 }], secFished: 60 }))
+    expect(fishPerMinuteInStations(stations)).toBe('60.00')
+  })
+
+  it('should return the correct amount of fish per minute when multiple stations', () => {
+    const stations = new Map()
+    stations.set(1, new Station({ observations: [{ count: 60 }], secFished: 60 }))
+    stations.set(2, new Station({ observations: [{ count: 60 }], secFished: 120 }))
+    expect(fishPerMinuteInStations(stations)).toBe('40.00')
+  })
+})
 
