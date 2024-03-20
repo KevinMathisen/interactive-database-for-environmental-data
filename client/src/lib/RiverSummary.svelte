85eb4dc5d120aed572b2ddb63a35703e7b1a956d
Kevin Nikolai Mathisen <kevinnm@stud.ntnu.no>
2024-03-20 14:15:13 +0100

    115 - docs: write comments for riversummary component

--- client/src/lib/RiverSummary.svelte
+++ client/src/lib/RiverSummary.svelte
@@ -1,35 +1,40 @@
 <script>
-    import { River } from '../models/River.js'
-    import CollapsibleSection from './CollapsibleSection.svelte'
-    import Button from './Button.svelte'
-    import RiverOverview from './RiverSummarySections/RiverOverview.svelte'
-    import RiverInfo from './RiverSummarySections/RiverInfo.svelte'
-    import { getStationsForRiver } from '../utils/dataManager.js'
-    import RiverStations from './RiverSummarySections/RiverStations.svelte'
+  import { River } from '../models/River.js'
+  import CollapsibleSection from './CollapsibleSection.svelte'
+  import Button from './Button.svelte'
+  import RiverOverview from './RiverSummarySections/RiverOverview.svelte'
+  import RiverInfo from './RiverSummarySections/RiverInfo.svelte'
+  import { getStationsForRiver } from '../utils/dataManager.js'
+  import RiverStations from './RiverSummarySections/RiverStations.svelte'
 
-    export let river = new River()
+  export let river = new River() // River to show
 
-    let stations = new Map()
+  let stations = new Map() // Stations under river
 
+  // Get stations for the river
   $: stations = getStationsForRiver(river)
 </script>
 
 <div class='container'>
 
   <div class="maincontent">
+    <!-- River name, project id, and date -->
     <CollapsibleSection title={river.name} collapsable={false}>
       <RiverOverview {river} />
     </CollapsibleSection>
 
+    <!-- River info such as observation data, crew, and comments -->
     <CollapsibleSection title="Info">
       <RiverInfo {river} {stations}/>
     </CollapsibleSection>
 
+    <!-- Stations under the river -->
     <CollapsibleSection title="Stasjoner">
       <RiverStations {stations} on:rowClick/>
     </CollapsibleSection>
   </div>
 
+  <!-- Buttons to show diagram and download data -->
   <div class="footer">
     <Button color="blue" type="medium">
       Diagram
@@ -78,9 +83,9 @@
     content: "";
     position: absolute;
     top: 0;
-    left: 10%; /* Center the line */
+    left: 10%; 
     width: 80%;
-    border-top: 2px solid #435768; /* Adjust color as needed */
+    border-top: 2px solid #435768; 
   }
 
 </style>
