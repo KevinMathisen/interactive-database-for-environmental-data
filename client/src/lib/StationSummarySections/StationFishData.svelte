4efb51ad20989dc18657db8faf4e5e4cc2b16995
Kevin Nikolai Mathisen <kevinnm@stud.ntnu.no>
2024-03-20 14:19:58 +0100

    116 - feat: create component for table with station fish data

---
+++ client/src/lib/StationSummarySections/StationFishData.svelte
@@ -0,0 +1,25 @@
+<script>
+  import SortableTable from "../SortableTable.svelte"
+  import { formatStationObservationsForTable } from "../../utils/formatData.js"
+
+  export let station // Station to show
+
+  let tableContent = {
+    headers: [],
+    rows: []
+  }
+
+  // Format the station observations for displaying in table
+  $: tableContent = formatStationObservationsForTable(station)
+
+</script>
+
+<!-- Table with fish data for the station, where the Sum row is highlighted -->
+<SortableTable
+  headers={tableContent.headers}
+  rows={tableContent.rows}
+  clickable=false
+  datatype="observation"
+  markRow='Sum'
+  on:rowClick
+/>
