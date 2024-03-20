c97b093156169a89118b2686c9028dc40cb36c08
martisgl <martisgl@stud.ntnu.no>
2024-03-20 14:59:38 +0100

    113 - test: added test for leafletmap component

---
+++ client/src/lib/LeafletMap.test.js
@@ -0,0 +1,25 @@
+import { render } from '@testing-library/svelte';
+import { describe, expect, it } from 'vitest';
+import LeafletMap from './LeafletMap.svelte';
+
+/**
+ * @vitest-environment jsdom
+ */
+
+describe('LeafletMap', () => {
+  it('map renders without causing error', () => {
+    const stations = [];
+    const rivers = [];
+    const dataType = 'station';
+
+    const { container } = render(LeafletMap, { 
+      props: { 
+        stations,
+        rivers,
+        dataType
+      } 
+    });
+
+    expect(container).toBeTruthy();
+  });
+});
