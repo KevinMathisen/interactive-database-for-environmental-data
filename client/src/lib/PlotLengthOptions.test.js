d1af57c8029e0fae111208d1b806d853bf0580f5
martisgl <martisgl@stud.ntnu.no>
2024-03-22 11:06:10 +0100

    113 - test: added test for PlotSpeciesOptions component

---
+++ client/src/lib/PlotLengthOptions.test.js
@@ -0,0 +1,36 @@
+import { render, fireEvent } from '@testing-library/svelte';
+import { describe, expect, it } from 'vitest';
+import PlotLengthOptions from './PlotLengthOptions.svelte';
+
+/**
+ * @vitest-environment jsdom
+ */
+
+describe('PlotLengthOptions', () => {
+  const { getByLabelText, getByPlaceholderText, rerender } 
+  = render(PlotLengthOptions, { showPlotB: false , intervallPlotB: 0, plotTypeB: 'histogram'});
+  it('binds showPlotB prop to checkbox', async () => {
+    
+    const checkbox = getByLabelText('Vis');
+
+    await fireEvent.click(checkbox);
+
+    expect(checkbox.checked).toBe(true);
+  });
+
+  it('binds intervallPlotB prop to number input', async () => {
+    const numberInput = getByPlaceholderText('mm');
+
+    await fireEvent.input(numberInput, { target: { value: '10' } });
+
+    expect(numberInput.value).toBe('10');
+  });
+
+  it('binds plotTypeB prop to radio input', async () => {
+    const radioInput = getByLabelText('Histogram');
+
+    await fireEvent.click(radioInput);
+
+    expect(radioInput.checked).toBe(true);
+  });
+});
