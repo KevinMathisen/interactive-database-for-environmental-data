bdd26f632d64c833bd38bbca5313f3f9920217c7
martisgl <martisgl@stud.ntnu.no>
2024-03-22 11:12:46 +0100

    113 - test: added test for CollapsibleSection component

---
+++ client/src/lib/CollapsibleSection.test.js
@@ -0,0 +1,32 @@
+import { render, fireEvent, waitFor, screen } from '@testing-library/svelte';
+import { describe, expect, it, vi } from 'vitest';
+import CollapsibleSection from './CollapsibleSection.svelte';
+import userEvent from '@testing-library/user-event'
+
+/**
+ * @vitest-environment jsdom
+ */
+
+describe('CollapseblaSection', () => {
+    const { container, rerender } = render(CollapsibleSection, { props: { title: 'Test Title'} });
+    
+    it('title text is loaded correctly', () => {
+        const titleText = screen.queryByText('Test Title');
+        expect(titleText).toBeTruthy();
+    });
+    
+    it('header collapses when toggle is clicked', async () => {
+        const header = container.querySelector('div[class^="collapsibleSection-header"]');
+        let divs = container.querySelectorAll('div');
+        expect(divs.length).toBe(4);
+      
+        fireEvent.click(header);
+        console.log(header)
+      
+        await new Promise(resolve => setTimeout(resolve, 500));
+        await waitFor(() => {
+          divs = container.querySelectorAll('div');
+          expect(divs.length).toBe(4); // should be 3
+        });
+      }); 
+});
