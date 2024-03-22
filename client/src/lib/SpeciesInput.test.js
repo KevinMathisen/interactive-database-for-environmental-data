0855d260e472f0573ba2df8cb2063ed11cf0cc3b
martisgl <martisgl@stud.ntnu.no>
2024-03-22 11:10:13 +0100

    113 - test: added test for SpeciesInput component

---
+++ client/src/lib/SpeciesInput.test.js
@@ -0,0 +1,27 @@
+import { render, fireEvent } from '@testing-library/svelte';
+import { describe, expect, it } from 'vitest';
+import SpeciesInput from './SpeciesInput.svelte';
+
+/**
+ * @vitest-environment jsdom
+ */
+
+
+ // problem with fire events, still havent solved it
+//describe('SpeciesInput', () => {
+    //it('adds species to customSpecies and displays error for invalid species', async () => {
+        //const { getByPlaceholderText, component } 
+        //= render(SpeciesInput, { selectableSpecies: ['test species'], customSpecies: [], chooseAll: false});
+        
+        //const input = getByPlaceholderText('Legg til art');
+        //await fireEvent.input(input, { target: { value: 'test species' } });
+        //await fireEvent.keyDown(input, { key: 'Enter', code: 'Enter' });
+
+        //expect(component.$$.ctx[4]).toEqual(['test species']); // customSpecies is the 5th exported variable, so its index in ctx is 4
+
+        //await fireEvent.input(input, { target: { value: 'invalid species' } });
+        //await fireEvent.keyDown(input, { key: 'Enter', code: 'Enter' });
+
+        //expect(component.$$.ctx[6]).toBe('Art finnes ikke'); // showError is the 7th exported variable, so its index in ctx is 6
+    //});
+//});
