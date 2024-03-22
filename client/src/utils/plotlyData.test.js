5a4ffa2cd6c4d950f9a2a25a3a0000f12a4e839a
Kevin Nikolai Mathisen <kevinnm@stud.ntnu.no>
2024-03-22 19:16:51 +0100

    119 - test: expand unit test for dataForBarAndPieChart(...) to cover when relative value is specified

--- client/src/utils/plotlyData.test.js
+++ client/src/utils/plotlyData.test.js
@@ -3,12 +3,13 @@ import {
   dataForBarAndPieChart,
   dataForHistogramAndBoxplot
 } from './plotlyData'
-import { getObservationsForRiver } from './dataManager'
+import { getObservationsForRiver, getStationsForRiver } from './dataManager.js'
 import { River } from '../models/River.js'
 import { Station } from '../models/Station.js'
 
 vi.mock('../utils/dataManager', () => ({
-  getObservationsForRiver: vi.fn()
+  getObservationsForRiver: vi.fn(),
+  getStationsForRiver: vi.fn()
 }))
 
 describe('test dataForBarAndPieChart function', () => {
@@ -200,6 +201,59 @@ describe('test dataForBarAndPieChart function', () => {
     expect(data).toEqual(expectedData)
   })
 
+  it('should return the correct relative river data when species and observations are given', () => {
+    // Mock getObservationsForRiver function to return observations for rivers given their id
+    getObservationsForRiver.mockImplementation((river) => {
+      if (river.id === 1) {
+        return [
+          { id: 1, species: 'species1', length: 10, count: 1 },
+          { id: 2, species: 'species2', length: 20, count: 2 },
+          { id: 3,species: 'species3', length: 10, count: 1 }
+        ]
+      } else if (river.id === 2) {
+        return [
+          { id: 1, species: 'species1', length: 10, count: 2 },
+          { id: 2, species: 'species2', length: 20, count: 1 }
+        ]
+      }
+    })
+    const rivers = new Map([
+      [1, new River({
+        id: 1,
+        name: 'Test river',
+        startDate: '2021-01-01'
+      })],
+      [2, new River({
+        id: 2,
+        name: 'Test river 2',
+        startDate: '2021-01-02'
+      })]
+    ])
+    const species = ['species1', 'species2', 'species3']
+    getStationsForRiver.mockReturnValue(new Map([
+      [1, new Station({
+        id: 1,
+        secFished: 120
+      })]
+    ]))
+    const expectedData = new Map([
+      ['Test river 2021-01-01', new Map([
+        ['species1', 0.5],
+        ['species2', 1],
+        ['species3', 0.5]
+      ])],
+      ['Test river 2 2021-01-02', new Map([
+        ['species1', 1],
+        ['species2', 0.5],
+        ['species3', 0]
+      ])]
+    ])
+
+    const data = dataForBarAndPieChart(rivers, 'river', species, false, false)
+
+    expect(data).toEqual(expectedData)
+  })
+
   it('should return count of 0 for each species if no observations are found for stations', () => {
     const stations = new Map([
       [1, new Station({
@@ -255,24 +309,9 @@ describe('test dataForBarAndPieChart function', () => {
         name: 'Test station',
         date: '2021-01-01',
         observations: [
-          {
-            id: 1,
-            species: 'species1',
-            length: 10,
-            count: 1
-          },
-          {
-            id: 2,
-            species: 'species2',
-            length: 20,
-            count: 2
-          },
-          {
-            id: 3,
-            species: 'species3',
-            length: 10,
-            count: 1
-          }
+          { id: 1, species: 'species1', length: 10, count: 1 },
+          { id: 2, species: 'species2', length: 20, count: 2 },
+          { id: 3, species: 'species3', length: 10, count: 1 }
         ]
       })],
       [2, new Station({
@@ -280,18 +319,8 @@ describe('test dataForBarAndPieChart function', () => {
         name: 'Test station 2',
         date: '2021-01-02',
         observations: [
-          {
-            id: 1,
-            species: 'species1',
-            length: 10,
-            count: 2
-          },
-          {
-            id: 2,
-            species: 'species2',
-            length: 20,
-            count: 1
-          }
+          { id: 1, species: 'species1', length: 10, count: 2 },
+          { id: 2, species: 'species2', length: 20, count: 1 }
         ]
       })]
     ])
@@ -321,24 +350,9 @@ describe('test dataForBarAndPieChart function', () => {
         name: 'Test station',
         date: '2021-01-01',
         observations: [
-          {
-            id: 1,
-            species: 'species1',
-            length: 10,
-            count: 1
-          },
-          {
-            id: 2,
-            species: 'species2',
-            length: 20,
-            count: 2
-          },
-          {
-            id: 3,
-            species: 'species3',
-            length: 10,
-            count: 1
-          }
+          { id: 1, species: 'species1', length: 10, count: 1 },
+          { id: 2, species: 'species2', length: 20, count: 2 },
+          { id: 3, species: 'species3', length: 10, count: 1 }
         ]
       })],
       [2, new Station({
@@ -346,18 +360,8 @@ describe('test dataForBarAndPieChart function', () => {
         name: 'Test station 2',
         date: '2021-01-02',
         observations: [
-          {
-            id: 1,
-            species: 'species1',
-            length: 10,
-            count: 2
-          },
-          {
-            id: 2,
-            species: 'species2',
-            length: 20,
-            count: 1
-          }
+          { id: 1, species: 'species1', length: 10, count: 2 },
+          { id: 2, species: 'species2', length: 20, count: 1 }
         ]
       })]
     ])
@@ -383,24 +387,9 @@ describe('test dataForBarAndPieChart function', () => {
         name: 'Test station',
         date: '2021-01-01',
         observations: [
-          {
-            id: 1,
-            species: 'species1',
-            length: 10,
-            count: 1
-          },
-          {
-            id: 2,
-            species: 'species2',
-            length: 20,
-            count: 2
-          },
-          {
-            id: 3,
-            species: 'species3',
-            length: 10,
-            count: 1
-          }
+          { id: 1, species: 'species1', length: 10, count: 1 },
+          { id: 2, species: 'species2', length: 20, count: 2 },
+          { id: 3, species: 'species3', length: 10, count: 1 }
         ]
       })],
       [2, new Station({
@@ -408,18 +397,8 @@ describe('test dataForBarAndPieChart function', () => {
         name: 'Test station 2',
         date: '2021-01-02',
         observations: [
-          {
-            id: 1,
-            species: 'species1',
-            length: 10,
-            count: 2
-          },
-          {
-            id: 2,
-            species: 'species2',
-            length: 20,
-            count: 1
-          }
+          { id: 1, species: 'species1', length: 10, count: 2 },
+          { id: 2, species: 'species2', length: 20, count: 1 }
         ]
       })]
     ])
@@ -438,6 +417,50 @@ describe('test dataForBarAndPieChart function', () => {
     const data = dataForBarAndPieChart(stations, 'station', species, true)
 
     expect(data).toEqual(expectedData)
+  })
+
+  it('should return the correct relative station data when species and observations are given', () => {
+    const stations = new Map([
+      [1, new Station({
+        id: 1,
+        name: 'Test station',
+        date: '2021-01-01',
+        secFished: 120,
+        observations: [
+          { id: 1, species: 'species1', length: 10, count: 1 },
+          { id: 2, species: 'species2', length: 20, count: 2 },
+          { id: 3, species: 'species3', length: 10, count: 1 }
+        ]
+      })],
+      [2, new Station({
+        id: 2,
+        name: 'Test station 2',
+        date: '2021-01-02',
+        secFished: 120,
+        observations: [
+          { id: 1, species: 'species1', length: 10, count: 2 },
+          { id: 2, species: 'species2', length: 20, count: 1 }
+        ]
+      })]
+    ])
+    const species = ['species1', 'species2', 'species3']
+    const expectedData = new Map([
+      ['Test station 2021-01-01', new Map([
+        ['species1', 0.5],
+        ['species2', 1],
+        ['species3', 0.5]
+      ])],
+      ['Test station 2 2021-01-02', new Map([
+        ['species1', 1],
+        ['species2', 0.5],
+        ['species3', 0]
+      ])]
+    ])
+
+    const data = dataForBarAndPieChart(stations, 'station', species, false, false)
+
+    expect(data).toEqual(expectedData)
+  
   })
 })
 
