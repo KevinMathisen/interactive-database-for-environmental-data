22852aa2b101a83cf224e42b6f3f2b1483cb281b
Kevin Nikolai Mathisen <kevinnm@stud.ntnu.no>
2024-03-25 13:57:38 +0100

    119 - feat: update piechart component to use plotting data instead of hardcoded values

--- client/src/lib/Diagrams/PieChartComponent.svelte
+++ client/src/lib/Diagrams/PieChartComponent.svelte
@@ -2,43 +2,56 @@
     // Imports the onMount function from svelte.
     import { onMount } from 'svelte'
 
+    export let plotData = new Map()
     let Plotly
 
     // Initializes the Plotly library when the component is mounted.
     onMount(async () => {
       Plotly = await import('plotly.js-dist-min')
-      // The data for the pie chart.
-      const stationData = [
-        { species: 'Ørret', count: 20 },
-        { species: 'Harr', count: 14 },
-        { species: 'Andre', count: 26 }
-      ]
+    })
 
-      // Adds the names and the count of each species into two separate arrays.
-      const labels = stationData.map(function (item) { return item.species })
-      const values = stationData.map(function (item) { return item.count })
+    $: if (Plotly && plotData.size > 0) {
+      console.log('drawing plot with plotData: ', plotData)
+      drawPlot(plotData)
+    }
 
-      // Adds the names and the count of the species into one array.
-      const textLabels = stationData.map(function (item) { return item.species + ' ' + item.count })
+    function drawPlot (plotData) {
 
-      // Sets the data on the axes, the chart type and the name of the slices in the pie chart.
-      const data = [{
-        values,
-        labels,
-        type: 'pie',
-        text: textLabels,
-        textposition: 'auto'
-      }]
+      // Find the rows and columns for the pie charts
+      // The number of rows and columns should be as close to each other as possible
+      const rows = Math.ceil(Math.sqrt(plotData.size))
+      const columns = Math.ceil(plotData.size / rows)
+
+      // Create pie charts for each observation point
+      const piecharts = []
+      plotData.forEach((observationPoint, name) => {
+        piecharts.push({
+          labels: Array.from(observationPoint.keys()),
+          values: Array.from(observationPoint.values()),
+          type: 'pie',
+          name,
+          //text: Array.from(observationPoint.entries(), ([key, value]) => `${key}: ${value}`),
+          textposition: 'auto',
+          domain: { // Find the position of the pie chart in the grid based on its index and the number of columns
+            row: Math.floor(piecharts.length / columns),
+            column: piecharts.length % columns
+          }
+        })
+      })
 
       // The title and fontsize.
-      const text = {
+      const layout = {
         title: 'FORDELING AV ARTER',
-        font: { size: 14 }
+        font: { size: 14 },
+        grid: {rows: rows, columns: columns}
       }
 
+      // Adjust the graph size according to the screen size.
+      const config = { responsive: true }
+
       // Creates the pie chart.
-      Plotly.newPlot('sectorOne', data, text)
-    })
+      Plotly.newPlot('sectorOne', piecharts, layout, config)
+    }
 </script>
 
 <!--Displays the pie chart with id "sectorOne"-->
