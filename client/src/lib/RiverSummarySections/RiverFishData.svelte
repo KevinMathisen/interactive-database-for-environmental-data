1d47531c725386d3d5b1755434f22f89fdf9d862
Kevin Nikolai Mathisen <kevinnm@stud.ntnu.no>
2024-04-05 17:05:40 +0200

    143 - feat: add fish data section to river summary

---
+++ client/src/lib/RiverSummarySections/RiverFishData.svelte
@@ -0,0 +1,25 @@
+<script>
+  import SortableTable from '../SortableTable.svelte'
+  import { formatRiverObservationsForTable } from '../../utils/formatData.js'
+
+  export let stations // Stations under river to get observations from
+
+  let tableContent = {
+    headers: [],
+    rows: []
+  }
+
+  // Format the river observations for displaying in table
+  $: tableContent = formatRiverObservationsForTable(stations)
+
+</script>
+
+<!-- Table with fish data for the river, where the Sum row is highlighted -->
+<SortableTable
+  headers={tableContent.headers}
+  rows={tableContent.rows}
+  clickable=false
+  datatype="observation"
+  markRow='sum'
+  on:rowClick
+/>
