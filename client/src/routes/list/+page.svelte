65f047de28a88c69d299c0032ba2258024224b45
Kevin Nikolai Mathisen <kevinnm@stud.ntnu.no>
2024-04-06 17:58:22 +0200

    117 - style: remove extra indentation in script tag on list page

--- client/src/routes/list/+page.svelte
+++ client/src/routes/list/+page.svelte
@@ -1,183 +1,183 @@
 <script> // List page logic here
-    import Filter from '$lib/filter.svelte'
-    import SortableTable from '$lib/SortableTable.svelte'
-    import SearchBar from '$lib/user-input/SearchBar.svelte'
-    import Sidebar from '$lib/Sidebar.svelte'
-    import RiverSummary from '$lib/RiverSummary.svelte'
-    import StationSummary from '$lib/StationSummary.svelte'
-    import { getRivers, getStations, getRiverSummary, getStationSummary } from '../../utils/dataManager.js'
-    import { getSelectableSpecies, filterRiversByDateAndSpecies, filterStationsByDateAndSpecies, filterRiversBySearch, filterStationsBySearch } from '../../utils/filterData.js'
-    import { formatRiversForTable, formatStationsForTable } from '../../utils/formatData.js'
-    import { riverStore } from '../../stores/riverStore.js'
-    import { stationStore } from '../../stores/stationStore.js'
-    import { onMount } from 'svelte'
-    import UserFeedbackMessage from '$lib/UserFeedbackMessage.svelte'
-    import { River } from '../../models/River.js'
-    import { Station } from '../../models/Station.js'
-    import { page } from '$app/stores'
+  import Filter from '$lib/filter.svelte'
+  import SortableTable from '$lib/SortableTable.svelte'
+  import SearchBar from '$lib/user-input/SearchBar.svelte'
+  import Sidebar from '$lib/Sidebar.svelte'
+  import RiverSummary from '$lib/RiverSummary.svelte'
+  import StationSummary from '$lib/StationSummary.svelte'
+  import { getRivers, getStations, getRiverSummary, getStationSummary } from '../../utils/dataManager.js'
+  import { getSelectableSpecies, filterRiversByDateAndSpecies, filterStationsByDateAndSpecies, filterRiversBySearch, filterStationsBySearch } from '../../utils/filterData.js'
+  import { formatRiversForTable, formatStationsForTable } from '../../utils/formatData.js'
+  import { riverStore } from '../../stores/riverStore.js'
+  import { stationStore } from '../../stores/stationStore.js'
+  import { onMount } from 'svelte'
+  import UserFeedbackMessage from '$lib/UserFeedbackMessage.svelte'
+  import { River } from '../../models/River.js'
+  import { Station } from '../../models/Station.js'
+  import { page } from '$app/stores'
 
-    let rivers // Rivers with coordinates
-    let stations // Stations with coordinates
-    let selectableSpecies // All unique species
+  let rivers // Rivers with coordinates
+  let stations // Stations with coordinates
+  let selectableSpecies // All unique species
 
-    let dataType // "river" or "station", chosen by user
-    let selectedSpecies // Species user wants to look at
-    let selectedStartDate // Start date for the time user wants to look at
-    let selectedEndDate // End date for the time user wants to look at
-    let searchQuery = '' // Search query from user
+  let dataType // "river" or "station", chosen by user
+  let selectedSpecies // Species user wants to look at
+  let selectedStartDate // Start date for the time user wants to look at
+  let selectedEndDate // End date for the time user wants to look at
+  let searchQuery = '' // Search query from user
 
-    let selectedRiver = new River // River the user has chosen
-    let selectedStation = new Station // Station the user has chosen
-    let riverStationPageTitle = ''
+  let selectedRiver = new River // River the user has chosen
+  let selectedStation = new Station // Station the user has chosen
+  let riverStationPageTitle = ''
 
-    let filteredRivers // Rivers filtered by date and species
-    let filteredStations // Stations filtered by date and species
-    let filteredBySearchRivers // Rivers filtered by search
-    let filteredBySearchStations // Stations filtered by search
-    let headers = [] // Header for the table
-    let rows = [] // Rows for the table
+  let filteredRivers // Rivers filtered by date and species
+  let filteredStations // Stations filtered by date and species
+  let filteredBySearchRivers // Rivers filtered by search
+  let filteredBySearchStations // Stations filtered by search
+  let headers = [] // Header for the table
+  let rows = [] // Rows for the table
 
-    $: riverStationPageTitle = dataType === 'river' ? 'Elvedata' : 'Stasjonsdata'
+  $: riverStationPageTitle = dataType === 'river' ? 'Elvedata' : 'Stasjonsdata'
 
-    onMount(async () => {
-      // Get rivers and stations from API
-      await Promise.all([getRivers(), getStations()])
-      getUrlParams()
-    })
+  onMount(async () => {
+    // Get rivers and stations from API
+    await Promise.all([getRivers(), getStations()])
+    getUrlParams()
+  })
 
-    // Get rivers and stations from stores
-    $: rivers = $riverStore
-    $: stations = $stationStore
-    $: selectableSpecies = getSelectableSpecies(rivers)
+  // Get rivers and stations from stores
+  $: rivers = $riverStore
+  $: stations = $stationStore
+  $: selectableSpecies = getSelectableSpecies(rivers)
 
-    // Find which rivers and stations to show on the map based on user input
-    $: filteredRivers = filterRiversByDateAndSpecies(rivers, selectedSpecies, selectedStartDate, selectedEndDate)
-    $: filteredStations = filterStationsByDateAndSpecies(stations, selectedSpecies, selectedStartDate, selectedEndDate)
+  // Find which rivers and stations to show on the map based on user input
+  $: filteredRivers = filterRiversByDateAndSpecies(rivers, selectedSpecies, selectedStartDate, selectedEndDate)
+  $: filteredStations = filterStationsByDateAndSpecies(stations, selectedSpecies, selectedStartDate, selectedEndDate)
 
-    $: filteredBySearchRivers = filterRiversBySearch(filteredRivers, searchQuery)
-    $: filteredBySearchStations = filterStationsBySearch(filteredStations, searchQuery)
+  $: filteredBySearchRivers = filterRiversBySearch(filteredRivers, searchQuery)
+  $: filteredBySearchStations = filterStationsBySearch(filteredStations, searchQuery)
 
-    /**
-     * Log the river/station clicked by the user,
-     * @param {Event} event - The click event
-     */
-    function handleRowClick (event) {
-      if (dataType === 'river') {
-        riverClicked(event)
-      } else if (dataType === 'station') {
-        stationClicked(event)
-      }
+  /**
+   * Log the river/station clicked by the user,
+   * @param {Event} event - The click event
+   */
+  function handleRowClick (event) {
+    if (dataType === 'river') {
+      riverClicked(event)
+    } else if (dataType === 'station') {
+      stationClicked(event)
+    }
+  }
+
+  /**
+   * Handles the click event on a station
+   * @param {Event} event - The click event
+   */
+  function stationClicked (event) {
+    getStationSummary(event.detail.id)
+      .then(_ => {
+        selectedRiver = new River()
+        selectedStation = stations.get(event.detail.id)
+      })
+  }
+
+  /**
+   * Handles the click event on a river
+   * @param {Event} event - The click event
+   */
+  function riverClicked (event) {
+    getRiverSummary(event.detail.id)
+      .then(_ => {
+        selectedStation = new Station()
+        selectedRiver = rivers.get(event.detail.id)
+      })
+  }
+
+  // Remove selected river or station when the user switches between data types
+  $: if (dataType === 'station') {
+    selectedRiver = new River()
+  } else if (dataType === 'river') {
+    selectedStation = new Station()
+  }
+
+  /**
+   * Toggles the river/station summary by resetting the selected river and station
+   */
+  function toggleSummaryPage () {
+    selectedRiver = new River()
+    selectedStation = new Station()
+  }
+
+  /**
+   * Create the header and data for the table
+   * @param {string} dataType - "river" or "station"
+   * @param {Map<number, object>} filteredBySearchRivers - Rivers filtered by search
+   * @param {Map<number, object>} filteredBySearchStations - Stations filtered by search
+   * @returns {{headers: string[], rows: string[][]}} - Headers and rows for the table
+   */
+  function createHeaderAndData (dataType, filteredBySearchRivers, filteredBySearchStations) {
+    if (dataType === 'river') {
+      return formatRiversForTable(filteredBySearchRivers)
+    } else if (dataType === 'station') {
+      return formatStationsForTable(filteredBySearchStations)
+    }
+    return { headers: [], rows: [] }
+  }
+
+  // Update the table when the user input changes
+  $: ({ headers, rows } = createHeaderAndData(dataType, filteredBySearchRivers, filteredBySearchStations))
+
+  // Update URL to reflect selected river or station
+  $: if (selectedRiver && selectedRiver.id || selectedStation && selectedStation.id) {
+    updateUrl(selectedRiver, selectedStation)
+  }
+
+  /**
+   * Updates the URL to reflect the selected river or station
+   * @param {River} selectedRiver - The selected river
+   * @param {Station} selectedStation - The selected station
+   */
+  function updateUrl(selectedRiver, selectedStation) {
+    let url = new URL(window.location.href)
+    if (selectedRiver.id) {
+      url.searchParams.set('river', selectedRiver.id)
+    } else {
+      url.searchParams.delete('river')
     }
 
-    /**
-     * Handles the click event on a station
-     * @param {Event} event - The click event
-     */
-    function stationClicked (event) {
-      getStationSummary(event.detail.id)
-        .then(_ => {
-          selectedRiver = new River()
-          selectedStation = stations.get(event.detail.id)
-        })
+    if (selectedStation.id) {
+      url.searchParams.set('station', selectedStation.id)
+    } else {
+      url.searchParams.delete('station')
     }
 
-    /**
-     * Handles the click event on a river
-     * @param {Event} event - The click event
-     */
-    function riverClicked (event) {
-      getRiverSummary(event.detail.id)
+    history.pushState({}, '', url)
+  }
+
+  /**
+   * Gets the river or station based on the URL parameters
+   */
+  function getUrlParams() {
+    let searchParams = new URLSearchParams($page.url.search)
+    let riverId = Number(searchParams.get('river'))
+    let stationId = Number(searchParams.get('station'))
+
+    if (riverId) {
+      dataType = 'river'
+      getRiverSummary(riverId)
         .then(_ => {
           selectedStation = new Station()
-          selectedRiver = rivers.get(event.detail.id)
+          selectedRiver = rivers.get(riverId)
+        })
+    } else if (stationId) {
+      dataType = 'station'
+      getStationSummary(stationId)
+        .then(_ => {
+          selectedRiver = new River()
+          selectedStation = stations.get(stationId)
         })
     }
-
-    // Remove selected river or station when the user switches between data types
-    $: if (dataType === 'station') {
-      selectedRiver = new River()
-    } else if (dataType === 'river') {
-      selectedStation = new Station()
-    }
-
-    /**
-     * Toggles the river/station summary by resetting the selected river and station
-     */
-    function toggleSummaryPage () {
-      selectedRiver = new River()
-      selectedStation = new Station()
-    }
-
-    /**
-     * Create the header and data for the table
-     * @param {string} dataType - "river" or "station"
-     * @param {Map<number, object>} filteredBySearchRivers - Rivers filtered by search
-     * @param {Map<number, object>} filteredBySearchStations - Stations filtered by search
-     * @returns {{headers: string[], rows: string[][]}} - Headers and rows for the table
-     */
-    function createHeaderAndData (dataType, filteredBySearchRivers, filteredBySearchStations) {
-      if (dataType === 'river') {
-        return formatRiversForTable(filteredBySearchRivers)
-      } else if (dataType === 'station') {
-        return formatStationsForTable(filteredBySearchStations)
-      }
-      return { headers: [], rows: [] }
-    }
-
-    // Update the table when the user input changes
-    $: ({ headers, rows } = createHeaderAndData(dataType, filteredBySearchRivers, filteredBySearchStations))
-
-    // Update URL to reflect selected river or station
-    $: if (selectedRiver && selectedRiver.id || selectedStation && selectedStation.id) {
-      updateUrl(selectedRiver, selectedStation)
-    }
-
-    /**
-     * Updates the URL to reflect the selected river or station
-     * @param {River} selectedRiver - The selected river
-     * @param {Station} selectedStation - The selected station
-     */
-    function updateUrl(selectedRiver, selectedStation) {
-      let url = new URL(window.location.href)
-      if (selectedRiver.id) {
-        url.searchParams.set('river', selectedRiver.id)
-      } else {
-        url.searchParams.delete('river')
-      }
-
-      if (selectedStation.id) {
-        url.searchParams.set('station', selectedStation.id)
-      } else {
-        url.searchParams.delete('station')
-      }
-
-      history.pushState({}, '', url)
-    }
-
-    /**
-     * Gets the river or station based on the URL parameters
-     */
-    function getUrlParams() {
-      let searchParams = new URLSearchParams($page.url.search)
-      let riverId = Number(searchParams.get('river'))
-      let stationId = Number(searchParams.get('station'))
-
-      if (riverId) {
-        dataType = 'river'
-        getRiverSummary(riverId)
-          .then(_ => {
-            selectedStation = new Station()
-            selectedRiver = rivers.get(riverId)
-          })
-      } else if (stationId) {
-        dataType = 'station'
-        getStationSummary(stationId)
-          .then(_ => {
-            selectedRiver = new River()
-            selectedStation = stations.get(stationId)
-          })
-      }
-    }
+  }
 </script>
 
 <!-- User feedback modal, invisible unless there is feedback to show to user -->
