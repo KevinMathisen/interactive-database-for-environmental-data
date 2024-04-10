119c2903a027a0e28478a6a0a3481fa02fd03911
Kevin Nikolai Mathisen <kevinnm@stud.ntnu.no>
2024-04-10 19:24:25 +0200

    138 - feat: only display species options when the graph is shown

--- client/src/lib/user-input/PlotSpeciesOptions.svelte
+++ client/src/lib/user-input/PlotSpeciesOptions.svelte
@@ -2,8 +2,8 @@
   import RadioInput from './RadioInput.svelte'
 
   export let showPlotA
-    export let showValueA
-    export let plotTypeA
+  export let showValueA
+  export let plotTypeA
 
   // Options when choosing y value
     const yValueOptions = [
@@ -23,10 +23,10 @@
   <input type="checkbox" id="showPlotA" name="showPlotA" bind:checked={showPlotA}>
 </label>
 
-<h4>Y-akse verdi</h4>
+{#if showPlotA}
+  <h4>Diagram type</h4>
+  <RadioInput name="plotTypeA" options={plotTypeOptions} bind:selected={plotTypeA}/>
 
-<RadioInput name="showPlotA" options={yValueOptions} bind:selected={showValueA}/>
-
-<h4>Diagram type</h4>
-
-<RadioInput name="plotTypeA" options={plotTypeOptions} bind:selected={plotTypeA}/>
+  <h4>Y-akse verdi</h4>
+  <RadioInput name="showPlotA" options={yValueOptions} bind:selected={showValueA}/>
+{/if}
