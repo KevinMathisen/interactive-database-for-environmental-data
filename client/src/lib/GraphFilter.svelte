4d3126b2175d44e9634d659ef7ab229e5a2e7560
Kevin Nikolai Mathisen <kevinnm@stud.ntnu.no>
2024-04-10 19:32:26 +0200

    138 - style: use correct indentation in graphFilter

--- client/src/lib/GraphFilter.svelte
+++ client/src/lib/GraphFilter.svelte
@@ -1,114 +1,114 @@
 <script>
-    import CollapsibleSection from './CollapsibleSection.svelte'
-    import SpeciesInput from './user-input/SpeciesInput.svelte'
-    import PlotSpeciesOptions from './user-input/PlotSpeciesOptions.svelte'
-    import PlotLengthOptions from './user-input/PlotLengthOptions.svelte'
-    import Button from './user-input/Button.svelte'
-    import { createEventDispatcher } from 'svelte'
-    const dispatch = createEventDispatcher()
+  import CollapsibleSection from './CollapsibleSection.svelte'
+  import SpeciesInput from './user-input/SpeciesInput.svelte'
+  import PlotSpeciesOptions from './user-input/PlotSpeciesOptions.svelte'
+  import PlotLengthOptions from './user-input/PlotLengthOptions.svelte'
+  import Button from './user-input/Button.svelte'
+  import { createEventDispatcher } from 'svelte'
+  const dispatch = createEventDispatcher()
 
-    export let selectedRivers
-    export let selectedStations
-    export let selectableSpecies = [] // Species the user can choose
-    export let dataType = 'river'
+  export let selectedRivers
+  export let selectedStations
+  export let selectableSpecies = [] // Species the user can choose
+  export let dataType = 'river'
 
-    export let selectedSpecies = []
-    export let includeOthers = false
+  export let selectedSpecies = []
+  export let includeOthers = false
 
-    export let showPlotA = true
-    export let showValueA = 'absolute'
-    export let plotTypeA = 'barchart'
+  export let showPlotA = true
+  export let showValueA = 'absolute'
+  export let plotTypeA = 'barchart'
 
-    export let showPlotB = true
-    export let intervallPlotB = 20
-    export let plotTypeB = 'histogram'
-    export let combineSpecies = false
+  export let showPlotB = true
+  export let intervallPlotB = 20
+  export let plotTypeB = 'histogram'
+  export let combineSpecies = false
 
-    let chooseAll = true // If the user wants to choose all species
-    let customSpecies = [] // Species the user has chosen
+  let chooseAll = true // If the user wants to choose all species
+  let customSpecies = [] // Species the user has chosen
 
-    // Species the user has choosen; either all or the custom ones
-    $: selectedSpecies = (chooseAll || customSpecies.length === 0 ) ? selectableSpecies : customSpecies
+  // Species the user has choosen; either all or the custom ones
+  $: selectedSpecies = (chooseAll || customSpecies.length === 0 ) ? selectableSpecies : customSpecies
 
-    /**
-     *
-     */
-    function handleSelectRiverStation () {
-      dispatch('selectRiverAndStation')
-    }
+  /**
+   *
+   */
+  function handleSelectRiverStation () {
+    dispatch('selectRiverAndStation')
+  }
 
 </script>
 
 <div class="main">
-    <!-- Input for opening selection of river or stations -->
-    <CollapsibleSection title="Velg elver/stasjoner">
-        <Button 
-            on:buttonClick={handleSelectRiverStation}
-            type="blue"
-            size="small"
-            >
-            Rediger
-            <img src="/editIcon.svg" alt="editIcon" height="30em" class="white-color">
-        </Button>
-        
-            {#if dataType === 'river' && selectedRivers.size !== 0}
-                <h4>Elver valgt</h4>
-                <ul>
-                {#each Array.from(selectedRivers.entries()) as [_, river]}
-                    <li>{river.name + ' ' + river.startDate}</li>
-                {/each}
-                </ul>
-            {:else if dataType === 'station' && selectedStations.size !== 0}
-                <p>Stasjoner valgt</p>
-                <ul>
-                {#each Array.from(selectedStations.entries()) as [_, station]}
-                    <li>{station.name + ' ' + station.date}</li>
-                {/each}
-                </ul>
-            {/if}
-    </CollapsibleSection>
+  <!-- Input for opening selection of river or stations -->
+  <CollapsibleSection title="Velg elver/stasjoner">
+    <Button 
+      on:buttonClick={handleSelectRiverStation}
+      type="blue"
+      size="small"
+      >
+      Rediger
+      <img src="/editIcon.svg" alt="editIcon" height="30em" class="white-color">
+    </Button>
+    
+      {#if dataType === 'river' && selectedRivers.size !== 0}
+        <h4>Elver valgt</h4>
+        <ul>
+        {#each Array.from(selectedRivers.entries()) as [_, river]}
+          <li>{river.name + ' ' + river.startDate}</li>
+        {/each}
+        </ul>
+      {:else if dataType === 'station' && selectedStations.size !== 0}
+        <p>Stasjoner valgt</p>
+        <ul>
+        {#each Array.from(selectedStations.entries()) as [_, station]}
+          <li>{station.name + ' ' + station.date}</li>
+        {/each}
+        </ul>
+      {/if}
+  </CollapsibleSection>
 
-    <!-- Input for choosing species -->
-    <CollapsibleSection title="Art">
-        <SpeciesInput {selectableSpecies} bind:chooseAll bind:customSpecies bind:includeOthers showIncludeOthers={true}/>
-    </CollapsibleSection>
+  <!-- Input for choosing species -->
+  <CollapsibleSection title="Art">
+    <SpeciesInput {selectableSpecies} bind:chooseAll bind:customSpecies bind:includeOthers showIncludeOthers={true}/>
+  </CollapsibleSection>
 
-    <!-- Input for choosing how plot A is displayed -->
-    <CollapsibleSection title="Fordeling av arter">
-        <PlotSpeciesOptions
-            bind:showPlotA
-            bind:showValueA
-            bind:plotTypeA />
-    </CollapsibleSection>
+  <!-- Input for choosing how plot A is displayed -->
+  <CollapsibleSection title="Fordeling av arter">
+    <PlotSpeciesOptions
+      bind:showPlotA
+      bind:showValueA
+      bind:plotTypeA />
+  </CollapsibleSection>
 
-    <!-- Input for choosing how plot B is displayed -->
-    <CollapsibleSection title="Fordeling av lengde">
-        <PlotLengthOptions
-            bind:showPlotB
-            bind:intervallPlotB
-            bind:plotTypeB
-            bind:combineSpecies />
-    </CollapsibleSection>
+  <!-- Input for choosing how plot B is displayed -->
+  <CollapsibleSection title="Fordeling av lengde">
+    <PlotLengthOptions
+      bind:showPlotB
+      bind:intervallPlotB
+      bind:plotTypeB
+      bind:combineSpecies />
+  </CollapsibleSection>
 
 </div>
 
 <style>
-    .main {
-        height: 100%;
-        width: 100%;
-        display: flex;
-        flex-direction: column;
-        justify-content: flex-start;
-        overflow: auto;
-    }
+  .main {
+    height: 100%;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    overflow: auto;
+  }
 
-    /* Transformes the icon color to white */
-    .white-color{
-        filter: invert(100%);
-    }
+  /* Transformes the icon color to white */
+  .white-color{
+    filter: invert(100%);
+  }
 
-    h4 {
-        margin-bottom: 0;
-    }
+  h4 {
+    margin-bottom: 0;
+  }
 
 </style>
