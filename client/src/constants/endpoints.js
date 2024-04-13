c2a3ee4013e2a9385c53a8887f95041153fd401b
Kevin Nikolai Mathisen <kevinnm@stud.ntnu.no>
2024-04-13 10:15:10 +0200

    146 - feat: add constants for authentication endpoints

--- client/src/constants/endpoints.js
+++ client/src/constants/endpoints.js
@@ -1,5 +1,6 @@
 export const POSTGREST_URL = import.meta.env.VITE_POSTGREST_URL
 export const UPLOAD_URL = import.meta.env.VITE_UPLOAD_URL
+export const AUTH_URL = import.meta.env.VITE_AUTH_URL
 
 export const RIVERS_ENDPOINT = '/river_with_species'
 export const STATIONS_ENDPOINT = '/station_with_species'
@@ -8,3 +9,8 @@ export const STATION_SUMMARY_ENDPOINT = '/station_summary'
 export const STATION_DOWNLOAD_ENDPOINT = '/station_download'
 
 export const UPLOAD_ENDPOINT = '/api/upload/'
+
+export const LOGIN_ENDPOINT = '/api/auth/login/'
+export const LOGOUT_ENDPOINT = '/api/auth/logout/'
+export const REFRESH_ENDPOINT = '/api/auth/token/refresh/'
+export const VERIFY_ENDPOINT = '/api/auth/token/verify/'
