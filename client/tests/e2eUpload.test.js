d7b2120ac4b6b085d54d585cb18d72bdf53b936b
Carl Petter Morch-Reiersen <carlpettermr@gmail.com>
2024-04-15 23:20:14 +0200

    67 - feat: removed hardcoded json to seperate file, fixed syncronization issue

--- client/tests/e2eUpload.test.js
+++ client/tests/e2eUpload.test.js
@@ -5,18 +5,12 @@ config({ path: '.env.test' })
 
 const UPLOAD_URL = 'http://localhost:8000'
 const UPLOAD_ENDPOINT = '/api/upload/'
-console.log(`UPLOAD_URL: ${UPLOAD_URL}${UPLOAD_ENDPOINT}`)
 
 
 test.describe('Tests the upload page', () => {
   test('Testing upload functioality and checking the format', async ({ page }) => {
-    /**
-     * Intercepts and mocks a POST request to the upload endpoint.
-     * @param {Route} route - The intercepted route.
-     */
-    await page.route(`${UPLOAD_URL}${UPLOAD_ENDPOINT}`, (route) => {
-      console.log(`Mocking POST request to ${UPLOAD_URL}${UPLOAD_ENDPOINT}`)
 
+    await page.route(`${UPLOAD_URL}${UPLOAD_ENDPOINT}`, (route) => {
       route.fulfill({
         status: 200,
         contentType: 'application/json',
@@ -28,16 +22,18 @@ test.describe('Tests the upload page', () => {
 
     try {
       await page.goto('/upload')
+      await page.waitForTimeout(500)
       const fileChooserPromise = page.waitForEvent('filechooser')
       await page.getByRole('link', { name: 'Bla gjennom Filer listIcon' }).click()
+      await page.waitForTimeout(1000)
       const fileChooser = await fileChooserPromise
       await fileChooser.setFiles('tests/test-data/elver.xlsx')
       await page.getByRole('main').getByRole('link', { name: 'Last opp listIcon' }).click()
+      await page.waitForTimeout(1000)
       expect(await page.locator('//h3')).toHaveText('Opplasting vellykket')
       await page.getByRole('button', { name: 'Close' }).click()
     } catch (error) {
-      logToFile('Test failed: ' + error.message)
       throw error
     }
-  }, { timeout: 5000 })
+  }, { timeout: 30000 })
 })
