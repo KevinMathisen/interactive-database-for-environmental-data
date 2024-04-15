b363f8da4e518b3a380f089b3b54f6b6abb2a3f2
Carl Petter Morch-Reiersen <carlpettermr@gmail.com>
2024-04-15 23:24:48 +0200

    67 - test: add file with json data for mocking of e2e tests

---
+++ client/tests/test-data/postgrestTestData.js
@@ -0,0 +1,748 @@
+
+
+export const riverWithSpeciesJson = {
+  id: 3,
+  name: 'Gaula',
+  pos: {
+    type: 'Point',
+    coordinates: [10.25962, 63.25848]
+  },
+  start_date: '2021-09-30',
+  end_date: '2021-09-30',
+  species: ['aure', 'laks', '3-pigga stingsild', 'skrubbe'],
+  project_id: 'Restaureringsprosjektet'
+}
+
+export const riverSummaryJson = {
+  id: 3,
+  name: 'Gaula',
+  start_date: '2021-09-30',
+  end_date: '2021-09-30',
+  project_id: 'Restaureringsprosjektet',
+  waterflow: 320,
+  boattype: 'Volt Maria',
+  skipper: 'Jon Museth',
+  crew: ['Tobias Holter', 'Erik Lie', ''],
+  comment: 'Testdatasett',
+  stations: [11]
+}
+
+export const stationDownloadJson = {
+  id: 11,
+  transect_length: null,
+  display: null,
+  gpx_file: null,
+  observations: [
+    {
+      id: 1,
+      species: 'aure',
+      length: 176,
+      count: 1
+    },
+    {
+      id: 1,
+      species: 'aure',
+      length: 128,
+      count: 1
+    },
+    {
+      id: 1,
+      species: 'aure',
+      length: 166,
+      count: 1
+    },
+    {
+      id: 1,
+      species: 'aure',
+      length: 171,
+      count: 1
+    },
+    {
+      id: 1,
+      species: 'aure',
+      length: 158,
+      count: 1
+    },
+    {
+      id: 1,
+      species: 'aure',
+      length: 92,
+      count: 1
+    },
+    {
+      id: 1,
+      species: 'aure',
+      length: 260,
+      count: 1
+    },
+    {
+      id: 1,
+      species: 'aure',
+      length: 128,
+      count: 1
+    },
+    {
+      id: 1,
+      species: 'aure',
+      length: 149,
+      count: 1
+    },
+    {
+      id: 1,
+      species: 'aure',
+      length: 115,
+      count: 1
+    },
+    {
+      id: 1,
+      species: 'aure',
+      length: 150,
+      count: 1
+    },
+    {
+      id: 1,
+      species: 'aure',
+      length: 154,
+      count: 1
+    },
+    {
+      id: 1,
+      species: 'aure',
+      length: 169,
+      count: 1
+    },
+    {
+      id: 1,
+      species: 'aure',
+      length: 150,
+      count: 1
+    },
+    {
+      id: 1,
+      species: 'aure',
+      length: 165,
+      count: 1
+    },
+    {
+      id: 1,
+      species: 'aure',
+      length: 115,
+      count: 1
+    },
+    {
+      id: 1,
+      species: 'aure',
+      length: 80,
+      count: 1
+    },
+    {
+      id: 1,
+      species: 'aure',
+      length: 152,
+      count: 1
+    },
+    {
+      id: 1,
+      species: 'aure',
+      length: 102,
+      count: 1
+    },
+    {
+      id: 1,
+      species: 'aure',
+      length: 99,
+      count: 1
+    },
+    {
+      id: 1,
+      species: 'aure',
+      length: 60,
+      count: 1
+    },
+    {
+      id: 1,
+      species: 'aure',
+      length: 58,
+      count: 1
+    },
+    {
+      id: 1,
+      species: 'aure',
+      length: 58,
+      count: 1
+    },
+    {
+      id: 1,
+      species: 'aure',
+      length: 180,
+      count: 1
+    },
+    {
+      id: 1,
+      species: 'aure',
+      length: 78,
+      count: 1
+    },
+    {
+      id: 1,
+      species: 'aure',
+      length: 126,
+      count: 1
+    },
+    {
+      id: 1,
+      species: 'aure',
+      length: 151,
+      count: 1
+    },
+    {
+      id: 1,
+      species: 'aure',
+      length: 55,
+      count: 1
+    },
+    {
+      id: 1,
+      species: 'aure',
+      length: 88,
+      count: 1
+    },
+    {
+      id: 1,
+      species: 'aure',
+      length: 99,
+      count: 1
+    },
+    {
+      id: 1,
+      species: 'aure',
+      length: 72,
+      count: 1
+    },
+    {
+      id: 1,
+      species: 'aure',
+      length: 78,
+      count: 1
+    },
+    {
+      id: 1,
+      species: 'aure',
+      length: 136,
+      count: 1
+    },
+    {
+      id: 1,
+      species: 'aure',
+      length: 196,
+      count: 1
+    },
+    {
+      id: 1,
+      species: 'aure',
+      length: 125,
+      count: 1
+    },
+    {
+      id: 1,
+      species: 'aure',
+      length: 60,
+      count: 1
+    },
+    {
+      id: 1,
+      species: 'aure',
+      length: 138,
+      count: 1
+    },
+    {
+      id: 1,
+      species: 'laks',
+      length: 118,
+      count: 1
+    },
+    {
+      id: 1,
+      species: 'laks',
+      length: 111,
+      count: 1
+    },
+    {
+      id: 1,
+      species: 'laks',
+      length: 89,
+      count: 1
+    },
+    {
+      id: 1,
+      species: 'laks',
+      length: 113,
+      count: 1
+    },
+    {
+      id: 1,
+      species: 'laks',
+      length: 95,
+      count: 1
+    },
+    {
+      id: 1,
+      species: 'laks',
+      length: 121,
+      count: 1
+    },
+    {
+      id: 1,
+      species: 'laks',
+      length: 122,
+      count: 1
+    },
+    {
+      id: 1,
+      species: 'laks',
+      length: 140,
+      count: 1
+    },
+    {
+      id: 1,
+      species: 'laks',
+      length: 118,
+      count: 1
+    },
+    {
+      id: 1,
+      species: 'laks',
+      length: 86,
+      count: 1
+    },
+    {
+      id: 1,
+      species: 'laks',
+      length: 85,
+      count: 1
+    },
+    {
+      id: 1,
+      species: 'laks',
+      length: 82,
+      count: 1
+    },
+    {
+      id: 1,
+      species: 'laks',
+      length: 101,
+      count: 1
+    },
+    {
+      id: 1,
+      species: 'laks',
+      length: 117,
+      count: 1
+    },
+    {
+      id: 1,
+      species: 'laks',
+      length: 78,
+      count: 1
+    },
+    {
+      id: 1,
+      species: 'laks',
+      length: 121,
+      count: 1
+    },
+    {
+      id: 1,
+      species: 'laks',
+      length: 117,
+      count: 1
+    },
+    {
+      id: 1,
+      species: 'laks',
+      length: 71,
+      count: 1
+    },
+    {
+      id: 1,
+      species: 'laks',
+      length: 43,
+      count: 1
+    },
+    {
+      id: 1,
+      species: 'laks',
+      length: 90,
+      count: 1
+    },
+    {
+      id: 1,
+      species: '3-pigga stingsild',
+      length: 25,
+      count: 1
+    },
+    {
+      id: 1,
+      species: '3-pigga stingsild',
+      length: 35,
+      count: 1
+    },
+    {
+      id: 1,
+      species: '3-pigga stingsild',
+      length: 29,
+      count: 1
+    },
+    {
+      id: 1,
+      species: 'aure',
+      length: 108,
+      count: 1
+    },
+    {
+      id: 1,
+      species: 'aure',
+      length: 90,
+      count: 1
+    },
+    {
+      id: 1,
+      species: 'aure',
+      length: 65,
+      count: 1
+    },
+    {
+      id: 1,
+      species: 'aure',
+      length: 108,
+      count: 1
+    },
+    {
+      id: 1,
+      species: 'aure',
+      length: 52,
+      count: 1
+    },
+    {
+      id: 1,
+      species: 'aure',
+      length: 135,
+      count: 1
+    },
+    {
+      id: 1,
+      species: 'aure',
+      length: 53,
+      count: 1
+    },
+    {
+      id: 1,
+      species: 'aure',
+      length: 165,
+      count: 1
+    },
+    {
+      id: 1,
+      species: 'aure',
+      length: 71,
+      count: 1
+    },
+    {
+      id: 1,
+      species: 'aure',
+      length: 69,
+      count: 1
+    },
+    {
+      id: 1,
+      species: 'aure',
+      length: 117,
+      count: 1
+    },
+    {
+      id: 1,
+      species: 'aure',
+      length: 64,
+      count: 1
+    },
+    {
+      id: 1,
+      species: 'aure',
+      length: 218,
+      count: 1
+    },
+    {
+      id: 1,
+      species: 'aure',
+      length: 126,
+      count: 1
+    },
+    {
+      id: 1,
+      species: 'aure',
+      length: 133,
+      count: 1
+    },
+    {
+      id: 1,
+      species: 'aure',
+      length: 87,
+      count: 1
+    },
+    {
+      id: 1,
+      species: 'aure',
+      length: 133,
+      count: 1
+    },
+    {
+      id: 1,
+      species: 'aure',
+      length: 115,
+      count: 1
+    },
+    {
+      id: 1,
+      species: 'aure',
+      length: 56,
+      count: 1
+    },
+    {
+      id: 1,
+      species: 'aure',
+      length: 65,
+      count: 1
+    },
+    {
+      id: 1,
+      species: 'aure',
+      length: 55,
+      count: 1
+    },
+    {
+      id: 1,
+      species: 'aure',
+      length: 104,
+      count: 1
+    },
+    {
+      id: 1,
+      species: 'aure',
+      length: 104,
+      count: 1
+    },
+    {
+      id: 1,
+      species: 'skrubbe',
+      length: 110,
+      count: 1
+    },
+    {
+      id: 1,
+      species: 'skrubbe',
+      length: 114,
+      count: 1
+    },
+    {
+      id: 1,
+      species: 'skrubbe',
+      length: 86,
+      count: 1
+    }
+  ]
+}
+
+export const stationSummaryJson = {
+  id: 11,
+  name: 'Gaula 1',
+  date: '2021-09-30',
+  time: '8:00',
+  river_id: 3,
+  description: 'Oppstrøms planlagt tiltaksområde, elvevenstre, Hofstadmoen',
+  comment: 'Elve-venstre, forbygning, 200-600 mm substrat',
+  river_type: 'Glattstrøm',
+  weather: 'Snø',
+  water_temp: 8.0,
+  air_temp: -1,
+  sec_fished: 550,
+  voltage: 1000,
+  pulse: 60,
+  conductivity: 77,
+  observations: [
+    {
+      id: 1,
+      species: 'aure',
+      length: 176,
+      count: 1
+    },
+    {
+      id: 1,
+      species: 'aure',
+      length: 128,
+      count: 1
+    },
+    {
+      id: 1,
+      species: 'aure',
+      length: 166,
+      count: 1
+    },
+    {
+      id: 1,
+      species: 'aure',
+      length: 171,
+      count: 1
+    },
+    {
+      id: 1,
+      species: 'aure',
+      length: 158,
+      count: 1
+    },
+    {
+      id: 1,
+      species: 'aure',
+      length: 92,
+      count: 1
+    },
+    {
+      id: 1,
+      species: 'aure',
+      length: 260,
+      count: 1
+    },
+    {
+      id: 1,
+      species: 'aure',
+      length: 128,
+      count: 1
+    },
+    {
+      id: 1,
+      species: 'aure',
+      length: 149,
+      count: 1
+    },
+    {
+      id: 1,
+      species: 'aure',
+      length: 115,
+      count: 1
+    },
+    {
+      id: 1,
+      species: 'aure',
+      length: 150,
+      count: 1
+    },
+    {
+      id: 1,
+      species: 'aure',
+      length: 154,
+      count: 1
+    },
+    {
+      id: 1,
+      species: 'aure',
+      length: 169,
+      count: 1
+    },
+    {
+      id: 1,
+      species: 'aure',
+      length: 150,
+      count: 1
+    },
+    {
+      id: 1,
+      species: 'aure',
+      length: 165,
+      count: 1
+    },
+    {
+      id: 1,
+      species: 'aure',
+      length: 115,
+      count: 1
+    },
+    {
+      id: 1,
+      species: 'aure',
+      length: 80,
+      count: 1
+    },
+    {
+      id: 1,
+      species: 'aure',
+      length: 152,
+      count: 1
+    },
+    {
+      id: 1,
+      species: 'aure',
+      length: 102,
+      count: 1
+    },
+    {
+      id: 1,
+      species: 'aure',
+      length: 99,
+      count: 1
+    },
+    {
+      id: 1,
+      species: 'aure',
+      length: 60,
+      count: 1
+    },
+    {
+      id: 1,
+      species: 'aure',
+      length: 58,
+      count: 1
+    },
+    {
+      id: 1,
+      species: 'aure',
+      length: 58,
+      count: 1
+    },
+    {
+      id: 1,
+      species: 'aure',
+      length: 180,
+      count: 1
+    },
+    {
+      id: 1,
+      species: 'skrubbe',
+      length: 114,
+      count: 1
+    },
+    {
+      id: 1,
+      species: 'skrubbe',
+      length: 86,
+      count: 1
+    }
+  ],
+  project_id: 'Restaureringsprosjektet'
+}
+
+export const stationWithSpeciesJson = {
+  id: 11,
+  name: 'Gaula 1',
+  start_pos: {
+    type: 'Point',
+    coordinates: [10.25962, 63.25848]
+  },
+  end_pos: {
+    type: 'Point',
+    coordinates: [10.26344, 63.25887]
+  },
+  date: '2021-09-30',
+  time: '08:00',
+  species: ['aure', 'laks', '3-pigga stingsild', 'skrubbe']
+}
+
+
