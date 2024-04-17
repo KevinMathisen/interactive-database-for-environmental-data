ac5452121a337de8264e5f9ccfb5295e150c3212
Kevin Nikolai Mathisen <kevinnm@stud.ntnu.no>
2024-04-17 18:32:24 +0200

    152 - feat: preload website icons

--- client/src/routes/+layout.svelte
+++ client/src/routes/+layout.svelte
@@ -1,9 +1,29 @@
+<svelte:head>
+  <!-- Preload icons -->
+  <link rel="preload" href="/closeIcon.svg" as="image">
+  <link rel="preload" href="/downloadIcon.svg" as="image">
+  <link rel="preload" href="/editIcon.svg" as="image">
+  <link rel="preload" href="/errorIcon.svg" as="image">
+  <link rel="preload" href="/fileSearchIcon.svg" as="image">
+  <link rel="preload" href="/graphIcon.svg" as="image">
+  <link rel="preload" href="/listIcon.svg" as="image">
+  <link rel="preload" href="/loginIcon.svg" as="image">
+  <link rel="preload" href="/logoutIcon.svg" as="image">
+  <link rel="preload" href="/mapIcon.svg" as="image">
+  <link rel="preload" href="/marker-icon-blue.png" as="image">
+  <link rel="preload" href="/marker-icon-orange.png" as="image">
+  <link rel="preload" href="/marker-icon-red.png" as="image">
+  <link rel="preload" href="/marker-shadow.png" as="image">
+  <link rel="preload" href="/successIcon.svg" as="image">
+  <link rel="preload" href="/uploadIcon.svg" as="image">
+</svelte:head>
+
 <script>
-    import Header from '$lib/header.svelte'
-    import '../global.css'
+  import Header from '$lib/header.svelte'
+  import '../global.css'
 </script>
 
 <Header />
 <main>
-    <slot />
+  <slot />
 </main>
