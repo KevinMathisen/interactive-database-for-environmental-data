03e894e13b6fe3042d889659940eafb7e8b84dbd
Kevin Nikolai Mathisen <kevinnm@stud.ntnu.no>
2024-04-22 15:21:44 +0200

    137 - test: update mock server to mock cookies

--- mock-server/server.js
+++ mock-server/server.js
@@ -3,16 +3,19 @@ const app = express();
 const fs = require('fs');
 const path = require('path');
 const cors = require('cors');
+const cookieParser = require('cookie-parser');
 const port = 8000; 
+
+const sessionid = 'token_value';
 
 // Middleware to enable CORS (Cross-Origin Resource Sharing) for development purposes
 // Without this the CORS policy: No 'Access-Control-Allow-Origin' header will block requests from the client
 app.use(cors());
-
 // Middleware to parse JSON
 app.use(express.json());
+// Middleware to parse cookies
+app.use(cookieParser());
 
-// Handle favicon requests
 app.get('/favicon.ico', (req, res) => res.status(204).end());
 
 // Handle requests for postgrest endpoint
@@ -20,6 +23,15 @@ app.get('/postgrest/:endpoint', (req, res) => {
 
 	// log the request
 	console.log(`GET request for ${req.params.endpoint}`);
+
+	// Read cookie
+	const sessionCookie = req.cookies.sessionid;
+
+	// Check if the cookie is valid
+	if (sessionCookie !== sessionid) {
+		res.sendStatus(401);
+		return;
+	}
 
 	// Extract the json file name from the request
 	const { endpoint } = req.params;
@@ -47,7 +59,7 @@ app.post('/api/auth/login/', (req, res) => {
 		// Set the Content-Type header
     res.setHeader('Content-Type', 'application/json');
     // Set the cookie
-    res.cookie('test', 'token_value', { httpOnly: true, sameSite: 'Strict' });
+    res.cookie('sessionid', sessionid, { httpOnly: true, sameSite: 'Strict' });
 		// Send JSON response
 		res.json({ user: { username: "admin" } });
 	} else {
@@ -61,6 +73,9 @@ app.post('/api/auth/logout/', (req, res) => {
 	// log the request
 	console.log(`POST request for /auth/logout`);
 
+	// Clear the cookie
+	res.clearCookie('sessionid');
+
 	// Send JSON response
 	res.json({ detail: 'success' });
 });
@@ -71,6 +86,15 @@ app.post('/api/auth/token/refresh/', (req, res) => {
 	// log the request
 	console.log(`POST request for /auth/refresh`);
 
+	// read cookie
+	const sessionCookie = req.cookies.sessionid;
+
+	// Check if the cookie is valid
+	if (sessionCookie !== sessionid) {
+		res.sendStatus(401);
+		return;
+	}
+
 	// Send JSON response
 	res.json({ detail: 'success' });
 });
@@ -80,6 +104,15 @@ app.post('/api/upload/', (req, res) => {
 
 	// log the request
 	console.log(`POST request for /upload`);
+
+	// read cookie
+	const sessionCookie = req.cookies.sessionid;
+
+	// Check if the cookie is valid
+	if (sessionCookie !== sessionid) {
+		res.sendStatus(401);
+		return;
+	}
 
 	// Send JSON response
 	res.json({ success: true });
