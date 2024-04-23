fadb7efdd6e3d5ba9b859aaf7db18b3a62ed3740
Kevin Nikolai Mathisen <kevinnm@stud.ntnu.no>
2024-04-23 19:06:59 +0200

    137 - feat: add infoIcon

--- client/src/constants/svgPaths.js
+++ client/src/constants/svgPaths.js
@@ -3,5 +3,6 @@ const svgBasePath = '/'
 export const SVG_PATHS = {
   ERROR: `${svgBasePath}errorIcon.svg`,
   SUCCESS: `${svgBasePath}successIcon.svg`,
-  CLOSE: `${svgBasePath}closeIcon.svg`
+  CLOSE: `${svgBasePath}closeIcon.svg`,
+  INFO: `${svgBasePath}infoIcon.svg`,
 }
